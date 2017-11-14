011638b5fe3768455f30c49c5091e101159228e2
Jeremy Hajek <hajek@iit.edu>
2017-11-14 10:50:17 -0600

    string for debug

--- itmo-444/Week-13/session.php
+++ itmo-444/Week-13/session.php
@@ -1,9 +1,12 @@
 <?php
 session_start();
+echo "The count is: ";
+
 if (!isset($_SESSION['count'])) {
   $_SESSION['count'] = 0;
 } else {
   $_SESSION['count']++;
 }
+
 echo "The count is: ". $_SESSION['count'];
 ?> 
