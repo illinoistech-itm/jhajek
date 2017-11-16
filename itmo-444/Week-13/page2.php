909471c3ec753e30f1295dd206afd223c072a2f0
Jeremy Hajek <hajek@iit.edu>
2017-11-16 10:20:53 -0600

    Added form field

--- itmo-444/Week-13/page2.php
+++ itmo-444/Week-13/page2.php
@@ -11,4 +11,16 @@ echo date('Y m d H:i:s', $_SESSION['time']);
 
 // You may want to use SID here, like we did in page1.php
 echo '<br /><a href="page1.php">page 1</a>';
+
+
 ?> 
+
+<form method="post">
+<fieldset>
+        <legend>What is Your Favorite Pet?</legend>
+                <input type="radio" name="animal" value="Cat" />Cats<br />
+                <input type="radio" name="animal" value="Dog" />Dogs<br />
+                <input type="radio" name="animal" value="Bird" />Birds<br />
+                <input type="submit" value="Submit now" />
+</fieldset>
+</form>
