e2c28e6bb454d626bbfa96e4582a6e602ec5053d
Jeremy Hajek <hajek@iit.edu>
2017-11-16 10:49:13 -0600

    added else

--- itmo-444/Week-13/page1.php
+++ itmo-444/Week-13/page1.php
@@ -15,7 +15,9 @@ echo '<br /><a href="page2.php">page 2</a><br />';
 
 if (!isset($_SESSION['myanimal'])) {
     echo "Not yet declared!\n";
-  } 
+  } else {
+      echo "Value is set.\n";
+  }
  if ($_SESSION['myanimal'] == 'cat') {
     echo '<span><img src="cat.jpg" /></span>';
   }  else {
