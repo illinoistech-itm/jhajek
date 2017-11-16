1f0c611ebeb4d95e68c271cfd3c1f330478a581c
Jeremy Hajek <hajek@iit.edu>
2017-11-16 11:13:51 -0600

    updated code

--- itmo-444/Week-13/page1.php
+++ itmo-444/Week-13/page1.php
@@ -5,7 +5,7 @@ session_start();
 
 echo 'Welcome to page #1';
 
-$_SESSION['favcolor'] = 'green';
+$_SESSION['favcolor'] = 'green';    
 $_SESSION['animal']   = 'cat';
 $_SESSION['time']     = time();
 
