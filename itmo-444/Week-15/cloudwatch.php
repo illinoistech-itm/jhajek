7bdb30243ca79e9e037f79cfdf68e22e1b477442
Jeremy Hajek <hajek@iit.edu>
2017-11-28 10:43:12 -0600

    Initial cw commit

--- itmo-444/Week-15/cloudwatch.php
+++ itmo-444/Week-15/cloudwatch.php
@@ -9,4 +9,23 @@ $cw = new Aws\CloudWatch\CloudWatchClient([
     'region'  => 'us-east-2'
 ]);
 
+$cwresult = $cw->getMetricStatistics([
+  'Dimensions' => [
+      [
+        'Name' => 'InstanceId',
+        'Value' => 'i-0f8ca7a39ae1815eb'
+      ],
+    ],
+    'EndTime' => strtotime('now'), // REQUIRED
+    'MetricName' => 'CPUUtilization', // REQUIRED
+    'Namespace' => 'AWS/EC2', // REQUIRED
+    'Period' => 300, // REQUIRED
+    'StartTime' => strtotime('-5 minutes'), // REQUIRED
+   // 'Statistics' => ['<string>', ...],
+   // 'Unit' => '
+
+]);
+
+print_r($cwresult);
+
 ?>
