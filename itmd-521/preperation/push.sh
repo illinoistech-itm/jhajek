e600ab8ee048677c49e1ee792e304177b909ef8e
Jeremy Hajek <hajek@iit.edu>
2018-01-22 09:55:50 -0600

    Script to prep data for Hadoop Cluster

---
+++ itmd-521/preperation/push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##################################################################
+#  This code will take the raw NCDC data which is stored in small gzipped files
+#  It will extract it and combine it into a single txt file, gzip it and upload to Hadoop, then bzip it and upload to hadoop
+#  
+####################################################################
+
+
+for i in {1949..1999}
+do
+
+    for file in ./$i/* ; do gunzip -c $file >> ./$i/$i.txt ; done
+    echo "Finished creating combined file of $i"
+    hadoop fs -mkdir /user/$USER/ncdc/$i
+    hadoop fs -copyFromLocal ./$i/$i.txt /user/$USER/ncdc/$i
+    gzip -kcv $i/$i.txt | hadoop fs -put - /user/$USER/ncdc/$i.txt.gz
+    bzip2 -zkv $i/$i.txt | hadoop fs -put - /user/$USER/ncdc/$i.txt.bz
+
+done
