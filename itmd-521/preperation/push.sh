c278b5faa488e11b3da5157334f974f9df1293b8
Jeremy Hajek <hajek@iit.edu>
2018-01-22 09:57:22 -0600

    changed to ckv

--- itmd-521/preperation/push.sh
+++ itmd-521/preperation/push.sh
@@ -14,7 +14,7 @@ do
     echo "Finished creating combined file of $i"
     hadoop fs -mkdir /user/$USER/ncdc/$i
     hadoop fs -copyFromLocal ./$i/$i.txt /user/$USER/ncdc/$i
-    gzip -kcv $i/$i.txt | hadoop fs -put - /user/$USER/ncdc/$i.txt.gz
+    gzip -ckv $i/$i.txt | hadoop fs -put - /user/$USER/ncdc/$i.txt.gz
     bzip2 -zkv $i/$i.txt | hadoop fs -put - /user/$USER/ncdc/$i.txt.bz
 
 done
