42716ef72a291c1b159cfba2fbb22d5637fc705d
Jeremy Hajek <hajek@iit.edu>
2018-01-23 10:40:27 -0600

    Used C style for loop to be able to pass in the positional parameters and still have the range

--- itmd-521/preperation/combine.sh
+++ itmd-521/preperation/combine.sh
@@ -3,20 +3,24 @@
 ##################################################
 # Script to combine a decade at a times txt files
 ##################################################
+# https://www.cyberciti.biz/faq/unix-linux-iterate-over-a-variable-range-of-numbers-in-bash/
 
-for i in {1990..1999}
+
+for (( i=$1; i<=$2; i++ ))
   do
-     cat $i/$i.txt >> $1.txt
+     cat $i/$i.txt >> $3.txt
   done    
   echo "Making decade directory"
-  hadoop fs -mkdir -p /user/$USER/ncdc/$1/
+  hadoop fs -mkdir -p /user/$USER/ncdc/$3/
   echo "Making gzip file"
-  gzip -kv $1.txt
+  gzip -kv $3.txt
   echo "putting to Hadoop Cluster"
-  hadoop fs -copyFromLocal ./$1.txt.gz /user/$USER/ncdc/$1/
-  rm $i.txt.gz
+  hadoop fs -copyFromLocal ./$3.txt.gz /user/$USER/ncdc/$3/
+  rm $3.txt.gz
   echo "Making bzip2 file"
-  bzip2 -zkv $1.txt
+  bzip2 -zkv $3.txt
   echo "putting to Hadoop Cluster"
-  hadoop fs -copyFromLocal ./$1.txt.gz /user/$USER/ncdc/$1/
-  rm $i.txt.bz
+  hadoop fs -copyFromLocal ./$3.txt.gz /user/$USER/ncdc/$3/
+  rm $3.txt.bz
+
+    
