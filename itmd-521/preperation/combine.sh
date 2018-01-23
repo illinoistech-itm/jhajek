64d89cf3aa3b9beb472ec8f1e0b43131ca7c6cfe
Jeremy Hajek <hajek@iit.edu>
2018-01-23 09:50:23 -0600

    Added full gzip and bzip process and deleting of resources

--- itmd-521/preperation/combine.sh
+++ itmd-521/preperation/combine.sh
@@ -4,7 +4,19 @@
 # Script to combine a decade at a times txt files
 ##################################################
 
-for i in {$1..$2}
+for i in {1990..1999}
   do
-     cat $i/$i.txt >> $3.txt
+     cat $i/$i.txt >> $1.txt
   done    
+  echo "Making decade directory"
+  hadoop fs -mkdir -p /user/$USER/ncdc/$1/
+  echo "Making gzip file"
+  gzip -kv $1.txt
+  echo "putting to Hadoop Cluster"
+  hadoop fs -copyFromLocal ./$1.txt.gz /user/$USER/ncdc/$1/
+  rm $i.txt.gz
+  echo "Making bzip2 file"
+  bzip2 -zkv $1.txt
+  echo "putting to Hadoop Cluster"
+  hadoop fs -copyFromLocal ./$1.txt.gz /user/$USER/ncdc/$1/
+  rm $i.txt.bz
