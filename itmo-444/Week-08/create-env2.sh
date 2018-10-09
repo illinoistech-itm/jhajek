13c884740a0437c3657113b20e644417e684cf28
Jeremy Hajek <hajek@iit.edu>
2018-10-09 12:27:31 -0500

    Added Reservation * to destroy.sh

--- itmo-444/Week-08/create-env2.sh
+++ itmo-444/Week-08/create-env2.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get install -y apache2 git
 
 cd /var/www/html
-sudo git clone https://github.com/illinoistech-itm/jhajek.git 
+sudo git clone https://github.com/illinoistech-itm/jhajek.git  1>> /home/ubuntu/out.log 2>> /home/ubuntu/err.log
 
 # command to copy index.html to /ver/www/html
--- itmo-444/Week-08/destroy-env.sh
+++ itmo-444/Week-08/destroy-env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
-VAR=`aws ec2 describe-instances --query 'Reservations[0].Instances[*].InstanceId'`
+#VAR=`aws ec2 describe-instances --query 'Reservations[0].Instances[*].InstanceId'`
+VAR=`aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId'`
 
 aws ec2 terminate-instances --instance-ids $VAR
 
