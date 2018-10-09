6f038ec0afa9c63a857b1c2630799f3497ee19b2
Jeremy Hajek <hajek@iit.edu>
2018-10-09 12:13:28 -0500

    Updated new-create2.sh to have sudo

--- itmo-444/Week-08/create-env2.sh
+++ itmo-444/Week-08/create-env2.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get install -y apache2 git
 
 cd /var/www/html
-git clone https://github.com/illinoistech-itm/jhajek.git 
+sudo git clone https://github.com/illinoistech-itm/jhajek.git 
 
 # command to copy index.html to /ver/www/html
