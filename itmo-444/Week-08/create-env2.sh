af98d73d722b492934a01bd5f1116ca751e688b7
Jeremy Hajek <hajek@iit.edu>
2018-10-09 12:08:58 -0500

    updated samples

--- itmo-444/Week-08/create-env2.sh
+++ itmo-444/Week-08/create-env2.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y apache2 git
 
+cd /var/www/html
 git clone https://github.com/illinoistech-itm/jhajek.git 
 
 # command to copy index.html to /ver/www/html
