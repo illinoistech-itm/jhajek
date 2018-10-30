72870cb13baca2347e103c45815a788be196c6da
Jeremy Hajek <hajek@iit.edu>
2018-10-29 23:33:54 -0500

    Added samples to chain mapper

---
+++ itmd-521/Week-11/ChainMapReducer.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class ChainMapReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+              @Override
+              protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+                             int sum = 0;
+                             for (IntWritable value : values) {sum += value.get();}
+                             context.write(key, new IntWritable(sum));
+              }
+}
