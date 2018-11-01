da409b1eae767e6ee51fc539b43c775870a5444c
Jeremy Hajek <hajek@iit.edu>
2018-11-01 11:50:49 -0500

    changed roles

--- itmo-444/Week-11/create.sh
+++ itmo-444/Week-11/create.sh
@@ -1,1 +1,2 @@
-aws ec2 run-instances --image-id ami-51537029 --count 3 --iam-instance-profile Name=developer --instance-type t2.micro --key-name x44-sp2 --security-groups inclass2018 --user-data file://create-env2.sh
+aws ec2 run-instances --image-id ami-51537029 --count 3 --iam-instance-profile Name=new-developer --instance-type t2.micro --key-name x44-sp2 --security-groups inclass2018 --user-data file://create-env2.sh
+ 
