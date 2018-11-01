8d7ca9f7fbd04266de3c814d424da10447964fd8
Jeremy Hajek <hajek@iit.edu>
2018-11-01 12:26:22 -0500

    updated php composer install

--- itmo-444/Week-11/create-env2.sh
+++ itmo-444/Week-11/create-env2.sh
@@ -4,27 +4,14 @@ sudo apt-get update
 sudo apt-get install -y apache2 git curl php php-simplexml unzip zip libapache2-mod-php
 
 # download and install php composer - https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
-
-EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+wget -q -O - https://composer.github.io/installer.sig
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE="$(php -r "echo hash_file('SHA384', 'composer-setup.php');")"
-
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
-fi
-
 php composer-setup.php --quiet
-RESULT=$?
-
 
 # download and install aws-skp-php library and package
 php -d memory_limit=-1 composer.phar require aws/aws-sdk-php
 
 # move vendor to /home/ubuntu
-
 mv vendor/ /home/ubuntu
 
 cd /var/www/html
