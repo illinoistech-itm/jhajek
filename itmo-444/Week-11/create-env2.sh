d55fa1eb798ece0c64afc76b58cfc35d5a79db33
Jeremy Hajek <hajek@iit.edu>
2018-11-01 11:38:13 -0500

    updated smaple code with comments

--- itmo-444/Week-11/create-env2.sh
+++ itmo-444/Week-11/create-env2.sh
@@ -3,12 +3,16 @@
 sudo apt-get update
 sudo apt-get install -y apache2 git curl php php-simplexml unzip zip libapache2-mod-php
 
-
+# download and install php composer
 wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php -- --quiet
 
+# download and install aws-skp-php library and package
 php -d memory_limit=-1 composer.phar require aws/aws-sdk-php
+
+# move vendor to /home/ubuntu
+
+mv vendor/ /home/ubuntu
 
 cd /var/www/html
 sudo git clone https://github.com/illinoistech-itm/jhajek.git  1>> /home/ubuntu/out.log 2>> /home/ubuntu/err.log
 
-# command to copy index.html to /ver/www/html
