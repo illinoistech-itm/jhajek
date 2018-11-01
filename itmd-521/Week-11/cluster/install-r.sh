fe03f2c2061ceaa5178987bff50551740589bff8
jeremy Hajek <hajek@iit.edu>
2018-11-01 11:17:25 -0500

    added cluster configs

---
+++ itmd-521/Week-11/cluster/install-r.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
+sudo apt-get update
+
+sudo apt-get install r-base
