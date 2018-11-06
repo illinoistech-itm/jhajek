194bd867449fccfb9a610c71ef3e4be85dfae56d
Jeremy Hajek <hajek@iit.edu>
2018-11-06 12:08:36 -0600

    added apcahe restart

--- itmo-444/Week-12/create-env.sh
+++ itmo-444/Week-12/create-env.sh
@@ -19,3 +19,5 @@ sudo php -d memory_limit=-1 composer.phar require aws/aws-sdk-php 1>> /home/ubun
 sudo git clone https://github.com/illinoistech-itm/jhajek.git  1>> /home/ubuntu/out.log 2>> /home/ubuntu/err.log
 
 sudo cp /home/ubuntu/jhajek/itmo-444/Week-12/*.php /var/www/html
+
+sudo systemctl reload apache2
