eef1cbb5c3726d583bcc26c888321786290e9dd3
Jeremy Hajek <hajek@iit.edu>
2018-11-05 21:34:34 -0600

    Updated samples

--- itmo-444/Week-12/connectS3.php
+++ itmo-444/Week-12/connectS3.php
@@ -5,7 +5,7 @@ require '/home/ubuntu/vendor/autoload.php';
 use Aws\S3\S3Client;
 
 $s3 = new Aws\S3\S3Client([
-    'profile' => 'default',
+   # 'profile' => 'default',  this line was causing trouble https://stackoverflow.com/questions/31919316/issue-using-an-iam-role-with-php-sdk?rq=1
     'version' => 'latest',
     'region' => 'us-east-2'
 ]);
