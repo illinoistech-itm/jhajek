67aef6e1d34d24390a0f3bb0d36ae5c0fab372be
Jeremy Hajek <hajek@iit.edu>
2018-11-06 12:36:56 -0600

    corrected regions

--- itmo-444/Week-12/connectRDS.php
+++ itmo-444/Week-12/connectRDS.php
@@ -6,7 +6,7 @@ use Aws\Rds\RdsClient;
 
 $rds = new Aws\Rds\RdsClient([
      'version' => 'latest',
-     'region' => 'us-east-2'
+     'region' => 'us-west-2'
  ]);
 
 //https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-rds-2014-10-31.html#describedbinstances
