38d2c0b35960224da0e00244efcedf89ae7740cb
Jeremy Hajek <hajek@iit.edu>
2018-11-06 12:30:51 -0600

    removed typo

--- itmo-444/Week-12/connectRDS.php
+++ itmo-444/Week-12/connectRDS.php
@@ -9,4 +9,10 @@ $rds = new Aws\Rds\RdsClient([
      'region' => 'us-east-2'
  ]);
 
- 
+//https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-rds-2014-10-31.html#describedbinstances
+$result = $rds->describeDBInstances([
+    'DBInstanceIdentifier' => 'jrh-inclass'
+]);
+
+
+echo $result['DBInstances'][0]['Endpoint']['Address'];
