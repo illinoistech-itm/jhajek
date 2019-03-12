ac20c13c53f6ac7dc99b55f8645bdaa988e7872f
Jeremy R Hajek <hajek@iit.edu>
2019-03-12 12:15:11 -0500

    updated class path

---
+++ itmd-521/config/set-path.sh
@@ -0,0 +1,8 @@
+cat << EOT >> ~/.bashrc 
+
+########## Inserted by User
+export JAVA_HOME=/usr
+export HADOOP_HOME=/home/vagrant/hadoop-2.8.5
+export PATH=PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+export HADOOP_CLASSPATH=/usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar
+EOT
