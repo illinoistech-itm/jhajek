ea4c22ab11824ee703bfb0dff54cceb0edd18593
Jeremy R Hajek <hajek@iit.edu>
2019-04-02 16:05:46 -0500

    Updated sample by removing comments

--- itmd-521/samples/chainMapSample.java
+++ itmd-521/samples/chainMapSample.java
@@ -2,10 +2,7 @@ org.apache.hadoop.conf.Configuration;
 org.apache.hadoop.conf.Configured;
 org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
 
-//Configuration conf = getConf();
 Configuration conf = new Configuration();
-
-//JobConf job = new JobConf(conf);
 
 Job job = new Job(conf);
 job.setJarByClass(MaxTemperature.class);
@@ -13,13 +10,6 @@ job.setJobName("Max temperature");
 
 FileInputFormat.addInputPath(job, new Path(args[0]));
 FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-//job.setJobName("ChainJob");
-//job.setInputFormat(TextInputFormat.class);
-//job.setOutputFormat(TextOutputFormat.class);
-
-//FileInputFormat.setInputPaths(job, in); 
-//FileOutputFormat.setOutputPath(job, out);
 
 // ***********************************************************************************************
 // Notes for configuring chain mappers
@@ -34,59 +24,6 @@ Configuration mapForKVPair=new Configuration(false);
 ChainMapper.addMapper(job, MaxTemperatureFilteredMapper.class, LongWritable.class, Text.class,Text.class,IntWritable.class,mapForKVPair);
 
 
-/*
-JobConf map1Conf = new JobConf(false);
-ChainMapper.addMapper(job,
-                      Map1.class,
-                      LongWritable.class,
-                      Text.class,
-                      Text.class,
-                      Text.class,
-                      true,
-                      map1Conf);
-
-JobConf map2Conf = new JobConf(false);
-ChainMapper.addMapper(job,
-                      BMap.class,
-                      Text.class,
-                      Text.class,
-                      LongWritable.class,
-                      Text.class,
-                      true,
-                      map2Conf);
- 
-JobConf reduceConf = new JobConf(false);
-ChainReducer.setReducer(job,
-                        Reduce.class,
-                        LongWritable.class,
-                        Text.class,
-                        Text.class,
-                        Text.class,
-                        true,
-                        reduceConf);
-
-JobConf map3Conf = new JobConf(false);
-ChainReducer.addMapper(job,
-                       Map3.class,
-                       Text.class,
-                       Text.class,
-                       LongWritable.class,
-                       Text.class,
-                       true,
-                       map3Conf);
-
-JobConf map4Conf = new JobConf(false);
-ChainReducer.addMapper(job,
-                       Map4.class,
-                       LongWritable.class,`
-                       Text.class,
-                       LongWritable.class,
-                       Text.class,
-                       true,
-                       map4Conf);
-*/ 
-
-// JobClient.runJob(job);
 job.setMapperClass(MaxTemperatureMapper.class);
 job.setReducerClass(MaxTemperatureReducer.class);
 
