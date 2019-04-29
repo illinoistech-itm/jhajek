5e041ec8f6adaec75f5dc46c98f53b568d61e647
Jeremy R Hajek <hajek@iit.edu>
2019-04-28 20:08:03 -0500

    fixed commands to native powershell

--- itmd-525-04/build-script.ps1
+++ itmd-525-04/build-script.ps1
@@ -1,11 +1,11 @@
 mkdir xenial64-1,xenial64-2,xenail64-3,xenial64-4
-cd xenial64-1
+Set-Location xenial64-1
 vagrant up
-cd ../xenial64-2
+Set-Location ../xenial64-2
 vagrant up
-cd ../xenail64-3
+Set-Location ../xenail64-3
 vagrant up
-cd ../xenial64-4
+Set-Location ../xenial64-4
 vagrant up
-cd ../
+Set-Location ../
 
