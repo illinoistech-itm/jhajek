66a69a339456b794745a4ddd7353ff19931557f3
Jeremy R Hajek <hajek@iit.edu>
2019-04-28 20:15:06 -0500

    Added inline script to install mongo and run bootstrap

---
+++ itmd-525-04/bootstrap2.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo hostnamectl set-hostname m2
+sudo apt-get update
+sudo apt-get install -y mongodb
---
+++ itmd-525-04/bootstrap3.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo hostnamectl set-hostname m3
+sudo apt-get update
+sudo apt-get install -y mongodb
