72223dc19349acdf86b368f86a44740c3725a462
Jeremy R Hajek <hajek@iit.edu>
2019-04-28 20:20:10 -0500

    Updated location of bootstrap script

--- itmd-525-04/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo hostnamectl set-hostname m1
-sudo apt-get update
-sudo apt-get install -y mongodb
-sudo systemctl disable mongodb
-sudo systemctl stop mongodb
--- itmd-525-04/build-script.sh
+++ itmd-525-04/build-script.sh
@@ -5,7 +5,7 @@ cd xenial64-1
 vagrant up
 cd ../xenial64-2
 vagrant up
-cd ../xenail64-3
+cd ../xenial64-3
 vagrant up
 cd ../xenial64-4
 vagrant up
