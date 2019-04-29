c3dfbfc03cb0863c050e16114c13c70c28447d4e
Jeremy R Hajek <hajek@iit.edu>
2019-04-28 20:16:27 -0500

    added files to stop default mongo from starting

--- itmd-525-04/bootstrap.sh
+++ itmd-525-04/bootstrap.sh
@@ -3,3 +3,5 @@
 sudo hostnamectl set-hostname m1
 sudo apt-get update
 sudo apt-get install -y mongodb
+sudo systemctl disable mongodb
+sudo systemctl stop mongodb
--- itmd-525-04/bootstrap3.sh
+++ itmd-525-04/bootstrap3.sh
@@ -3,3 +3,5 @@
 sudo hostnamectl set-hostname m3
 sudo apt-get update
 sudo apt-get install -y mongodb
+sudo systemctl disable mongodb
+sudo systemctl stop mongodb
