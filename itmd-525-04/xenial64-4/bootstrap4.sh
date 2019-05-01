13d41f10375a3fa872dfb5a3da23d78da552acfd
Jeremy R Hajek <hajek@iit.edu>
2019-05-01 16:57:40 -0500

    updated etc hosts to reflect changes

--- itmd-525-04/xenial64-4/bootstrap4.sh
+++ itmd-525-04/xenial64-4/bootstrap4.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-
 sudo apt-get update
 sudo apt-get install -y mongodb
 sudo systemctl disable mongodb
 sudo systemctl stop mongodb
 sudo hostnamectl set-hostname m4
+sudo sed -i -e '127\.0\.1\.1    ubuntu-xenial ubuntu-xenial/127\.0\.1\.1 m4 m4' /etc/hosts
