a9d36990ccce19ae32cf793946fbb78472e5aef6
Jeremy R Hajek <hajek@iit.edu>
2019-05-01 16:57:21 -0500

    Updated etc hosts to reflect changes

--- itmd-525-04/xenial64-3/bootstrap3.sh
+++ itmd-525-04/xenial64-3/bootstrap3.sh
@@ -8,3 +8,4 @@ sudo systemctl stop mongodb
 sudo mkdir -p /home/vagrant/data/rs3
 sudo chown -R vagrant:vagrant /home/vagrant/data
 sudo hostnamectl set-hostname m3
+sudo sed -i -e '127\.0\.1\.1    ubuntu-xenial ubuntu-xenial/127\.0\.1\.1 m3 m3' /etc/hosts
