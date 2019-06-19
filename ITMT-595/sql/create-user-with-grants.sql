aa893ce438a23e550c14f8476747c60ba7e39289
Jeremy R Hajek <hajek@iit.edu>
2019-06-19 11:35:07 -0500

    removed files not needed

--- ITMT-595/sql/create-user-with-grants.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'worker'@'$ACCESSFROMIP' IDENTIFIED BY 'yourpasswordhere';
-GRANT ALL ON *.* TO 'yourname'@'IPGOESHERE';
-
--- My example would be
--- CREATE USER 'worker'@'192.168.1.239' IDENTIFIED BY 'ilovebunnies';
--- GRANT ALL ON *.* TO 'worker'@'192.168.1.239';
-
