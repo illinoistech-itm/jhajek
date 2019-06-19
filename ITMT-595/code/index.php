dcfba5fb7dba73e902db249290a76fa7ba7d3ac3
Jeremy R Hajek <hajek@iit.edu>
2019-06-19 10:43:24 -0500

    added sample code

---
+++ ITMT-595/code/index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "hello from php";
+
+?>
+
+<html>
+<head><title>Hello World</title></head>
+<body>
+
+<h1>Hello Wurld</h1>
+
+Yes I know it is mispelled but I think it looks better with a 'u' - don't you ag
+ree?
+<br />
+<a href='test-select.php'>test-select.php</a>
+<br />
+
+</body>
+</html>
