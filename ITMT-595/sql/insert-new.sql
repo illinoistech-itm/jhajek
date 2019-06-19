85208470689bca0711db882e3a54730e6e6add18
Jeremy R Hajek <hajek@iit.edu>
2019-06-19 11:09:36 -0500

    added docker entrypoint

---
+++ ITMT-595/sql/insert-new.sql
@@ -0,0 +1,3 @@
+Use comments;
+
+INSERT INTO comment (id, Title, PosterName, Content) Values (0,"New Idea","Abraham Lincoln","Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal."),(0,"Someone is wrong on the internet","Annie Mouse","https://www.xkcd.com/386/"),(0,"Presidential Quote","President James Madison","If men were angels, no government would be necessary");
