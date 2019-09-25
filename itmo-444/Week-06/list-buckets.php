e848d67e7d8fb1222d34b3d16e57692973a6cea8
Jeremy R Hajek <hajek@iit.edu>
2019-09-25 14:49:18 -0500

    created sample

---
+++ itmo-444/Week-06/list-buckets.php
@@ -0,0 +1,19 @@
+<?php
+require '~/vendor/autoload.php';
+
+use Aws\S3\S3Client;  
+use Aws\Exception\AwsException;
+
+$s3Client = new S3Client([
+    'profile' => 'default',
+    'region' => 'us-west-2',
+    'version' => '2006-03-01'
+]);
+
+//Listing all S3 Bucket
+$buckets = $s3Client->listBuckets();
+foreach ($buckets['Buckets'] as $bucket) {
+    echo $bucket['Name'] . "\n";
+}
+
+?>
