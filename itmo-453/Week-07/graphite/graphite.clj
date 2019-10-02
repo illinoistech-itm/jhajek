001e42f3e651d63902c390bd38cee03ef04a49c7
Jeremy R Hajek <hajek@iit.edu>
2019-10-02 00:56:18 -0500

    removed )

--- itmo-453/Week-07/graphite/graphite.clj
+++ itmo-453/Week-07/graphite/graphite.clj
@@ -5,4 +5,4 @@
 (defn add-environment-to-graphite [event] (str "productiona.hosts.", (riemann.graphite/graphite-path-percentiles event)))
 
 (def graph (async-queue! :graphite {:queue-size 1000}
-            (graphite {:host "graphitea" :path add-environment-to-graphite}))))
+            (graphite {:host "graphitea" :path add-environment-to-graphite})))
