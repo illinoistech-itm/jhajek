08f40f85b36c5eaa3e2112ce95a35e151ebe7d2a
Jeremy R Hajek <hajek@iit.edu>
2019-10-28 15:47:31 -0500

    added samples

---
+++ itmo-444/Week-11/modpng.php
@@ -0,0 +1,30 @@
+<?php
+function LoadPng($imgname)
+{
+    /* Attempt to open */
+    $im = @imagecreatefrompng($imgname);
+
+    /* See if it failed */
+    if(!$im)
+    {
+        /* Create a black image */
+        $im  = imagecreatetruecolor(150, 30);
+        $bgc = imagecolorallocate($im, 255, 255, 255);
+        $tc  = imagecolorallocate($im, 0, 0, 0);
+
+        imagefilledrectangle($im, 0, 0, 150, 30, $bgc);
+
+        /* Output an error message */
+        imagestring($im, 1, 5, 5, 'Error loading ' . $imgname, $tc);
+    }
+
+    return $im;
+}
+
+header('Content-Type: image/png');
+
+$img = LoadPng("https://php-jrh-547d759e72042.s3.amazonaws.com/tmp/Setting-icon.png");
+
+imagepng($img,"/tmp/g5.png");
+//imagedestroy($img);
+?>
