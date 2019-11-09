52b2c605e8f9615fc79794e8928847748376edca
Jeremy R. Hajek <hajek@iit.edu>
2019-11-08 19:56:27 -0600

    added command lines

--- cluster/d2/annotations-load.py
+++ cluster/d2/annotations-load.py
@@ -18,6 +18,7 @@ from detectron2.data import MetadataCatalog
 
 #command prompts values
 imagein=sys.argv[1]
+cfgin=sys.argv[2] + "detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
 
 # read image
 im = cv2.imread(imagein)
@@ -28,7 +29,7 @@ cv2.waitKey(5000)
 cv2.destroyAllWindows()
 # do detection
 cfg = get_cfg()
-cfg.merge_from_file("/home/conntroller/detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.merge_from_file(cfgin)
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
 # Find a model from detectron2's model zoo. You can either use the https://dl.fbaipublicfiles.... url, or use the following shorthand
 cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
