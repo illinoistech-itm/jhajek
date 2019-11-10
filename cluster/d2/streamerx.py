4e0475c30fe5569b4e88dc6878072b1c7caf5ebb
Jeremy R. Hajek <hajek@iit.edu>
2019-11-09 18:29:59 -0600

    changed index

--- cluster/d2/streamerx.py
+++ cluster/d2/streamerx.py
@@ -74,7 +74,7 @@ while counter != count:
 
 camera2.release()
 counter = 0
-camera3 = cv2.VideoCapture(6)  # init the third camera
+camera3 = cv2.VideoCapture(5)  # init the third camera
 
 while counter != count:
     print("camera 3")
