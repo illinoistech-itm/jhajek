d7bbd15b711ac1e3128414cf2a1f0d6ea772c586
Jeremy R. Hajek <hajek@iit.edu>
2019-11-09 18:40:27 -0600

    sped up the speaking feedback

--- cluster/d2/annotations-load-viewer.py
+++ cluster/d2/annotations-load-viewer.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     """ RATE"""
     rate = engine.getProperty('rate')   # getting details of current speaking rate
     print (rate)                        #printing current voice rate
-    engine.setProperty('rate', 175)     # setting up new voice rate
+    engine.setProperty('rate', 200)     # setting up new voice rate
 
     # COCO dataset categories
     dataset=['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
