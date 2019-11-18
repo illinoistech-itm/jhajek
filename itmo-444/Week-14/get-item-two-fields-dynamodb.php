bc220cac73d91705f94b9e5c3cfb198228843007
Jeremy R Hajek <hajek@iit.edu>
2019-11-18 16:46:31 -0600

    Updated output

--- itmo-444/Week-14/get-item-two-fields-dynamodb.php
+++ itmo-444/Week-14/get-item-two-fields-dynamodb.php
@@ -25,7 +25,7 @@ print_r($result);
 
 echo "Results: " . "\n";
 echo "S3finished URL: " . "\n";
-echo $result['S3finishedurl'][0];
+echo $result['S3finishedurl']['S'];
 
 /* ProjectionExpression - A string that identifies one or more attributes to retrieve from the table. 
 These attributes can include scalars, sets, or elements of a JSON document. The attributes in the expression must
