332827190b2100b67909091eae0eb0c36d105201
Jeremy R Hajek <hajek@iit.edu>
2019-11-17 23:21:06 -0600

    fixed bools

--- itmo-444/Week-14/put-item-dynamodb-hard-code-values.php
+++ itmo-444/Week-14/put-item-dynamodb-hard-code-values.php
@@ -25,8 +25,8 @@ $result = $client->putItem([
     'Filename' => ['S' => substr(md5(rand()), 0, 7)],
     'S3rawurl' => ['S' => "S3://..."],
     'S3finishedurl' => ['S' => ''],     
-    'Status' => ['B' => FALSE],
-    'Issubscribed' => ['B' => FALSE]     
+    'Status' => ['Bool' => false],
+    'Issubscribed' => ['Bool' => false]     
     ]
    
     ]);
