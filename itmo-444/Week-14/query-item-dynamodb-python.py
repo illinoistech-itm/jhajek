9b50ae617985e3b8e1c6e7e1c7df01db95383506
Jeremy R Hajek <hajek@iit.edu>
2019-11-20 01:03:56 -0600

    added table name

--- itmo-444/Week-14/query-item-dynamodb-python.py
+++ itmo-444/Week-14/query-item-dynamodb-python.py
@@ -6,7 +6,7 @@ dynamodb = boto3.resource('dynamodb')
 
 # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#getting-an-item
 
-response = table.get_item(
+response = RecordsXYZ.get_item(
     Key={
         'Receipt': '5dd4d65c2099f',
         'Email': 'hajek@iit.edu'
