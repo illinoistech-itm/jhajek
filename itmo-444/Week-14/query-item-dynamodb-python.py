54a3fb8ebf8709fa7bac608f97d7a0627da2b76f
Jeremy R Hajek <hajek@iit.edu>
2019-11-20 01:03:07 -0600

    updated query

---
+++ itmo-444/Week-14/query-item-dynamodb-python.py
@@ -0,0 +1,17 @@
+import boto3
+
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#getting-an-item
+
+response = table.get_item(
+    Key={
+        'Receipt': '5dd4d65c2099f',
+        'Email': 'hajek@iit.edu'
+    }
+)
+item = response['Item']
+print(item)
+
