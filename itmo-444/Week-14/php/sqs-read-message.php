1494802772cc763106087fb0487d03be7b1617ba
Jeremy R Hajek <hajek@iit.edu>
2019-11-20 07:42:53 -0600

    added sqs full example

--- itmo-444/Week-14/php/sqs-read-message.php
+++ itmo-444/Week-14/php/sqs-read-message.php
@@ -19,6 +19,16 @@ $result = $client->listQueues([
 print_r($result['QueueUrls']);
 $queueURL = $result['QueueUrls'][0];
 
+# https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-sqs-2012-11-05.html#getqueueurl
+// or this is a valid approach too
+$result = $client->getQueueUrl([
+    'QueueName' => 'inclass-jrh', // REQUIRED
+    //'QueueOwnerAWSAccountId' => '<string>',  // optional
+]);
+
+print_r($result['QueueURL']);
+$URL = $result['QueueURL'];
+
 # https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-sqs-2012-11-05.html#sendmessage
 
 $resultsqs = $client->sendMessage([
@@ -27,3 +37,22 @@ $resultsqs = $client->sendMessage([
 ]);
 
 print_r($resultsqs);
+
+# https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-sqs-2012-11-05.html#receivemessage
+$result = $client->receiveMessage([
+    'QueueUrl' => $URL, // REQUIRED
+    'VisibilityTimeout' => 300,
+    'WaitTimeSeconds' => 30,
+]);
+
+print_r($result['Messages']);
+print_r($result['Messages'][0]['Body']);
+print_r($result['Messages'][0]['MessageId']);
+print_r($result['Messages'][0]['ReceiptHandle']);
+$handle = $result['Messages'][0]['ReceiptHandle'];
+# https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-sqs-2012-11-05.html#deletemessage
+
+$result = $client->deleteMessage([
+    'QueueUrl' => $URL, // REQUIRED
+    'ReceiptHandle' => $handle, // REQUIRED
+]);
