634d909fb53e96194214b0101326bafbf00cf2b4
Jeremy R Hajek <hajek@iit.edu>
2019-11-20 14:26:34 -0600

    removed $

--- itmo-444/Week-14/bash/delete-sns-topic.sh
+++ itmo-444/Week-14/bash/delete-sns-topic.sh
@@ -3,5 +3,5 @@
 # https://docs.aws.amazon.com/cli/latest/reference/sns/delete-topic.html
 # https://docs.aws.amazon.com/cli/latest/reference/sns/list-topics.html
 
-$TOPICARN=`aws sns list-topics`
+TOPICARN=`aws sns list-topics`
 aws sns delete-topic --topic-arn $TOPICARN | awk {'print $2'}  
