5903271359be88d62320fe06ae5ef2d3d37ed40d
Jeremy R Hajek <hajek@iit.edu>
2019-11-20 00:58:03 -0600

    updated s3 download

--- itmo-444/Week-14/render-image.py
+++ itmo-444/Week-14/render-image.py
@@ -2,12 +2,12 @@ from PIL import Image, ImageFilter
 
 # https://docs.python-guide.org/scenarios/imaging/
 # https://stackoverflow.com/questions/1386352/pil-thumbnail-and-end-up-with-a-square-image
-#Read image
+#Read image and create a thumbnail
 im = Image.open( 'data.png' )
-size = (36, 36)
+size = (100, 100)
 im.thumbnail(size, Image.ANTIALIAS)
 background = Image.new('RGBA', size, (255, 255, 255, 0))
 background.paste(
     im, (int((size[0] - im.size[0]) / 2), int((size[1] - im.size[1]) / 2))
 )
-background.save("output.png")
+background.save("thunbnail.png")
