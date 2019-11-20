2f70f01b92aa367ae61a58d9cc2a63b4b8e23eb7
Jeremy R Hajek <hajek@iit.edu>
2019-11-20 14:28:42 -0600

    spaces

--- itmo-444/Week-14/bash/delete-sns-topic.sh
+++ itmo-444/Week-14/bash/delete-sns-topic.sh
@@ -4,4 +4,4 @@
 # https://docs.aws.amazon.com/cli/latest/reference/sns/list-topics.html
 
 TOPICARN=`aws sns list-topics | awk {'print $2'}`
-aws sns delete-topic --topic-arn $TOPICARN   
+aws sns delete-topic --topic-arn $TOPICARN
