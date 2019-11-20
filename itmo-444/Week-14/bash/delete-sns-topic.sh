9e55dd8c2abfb7273f4af40b20dbaed9f9d82800
Jeremy R Hajek <hajek@iit.edu>
2019-11-20 14:28:26 -0600

    fixed awk location

--- itmo-444/Week-14/bash/delete-sns-topic.sh
+++ itmo-444/Week-14/bash/delete-sns-topic.sh
@@ -3,5 +3,5 @@
 # https://docs.aws.amazon.com/cli/latest/reference/sns/delete-topic.html
 # https://docs.aws.amazon.com/cli/latest/reference/sns/list-topics.html
 
-TOPICARN=`aws sns list-topics`
-aws sns delete-topic --topic-arn $TOPICARN | awk {'print $2'}  
+TOPICARN=`aws sns list-topics | awk {'print $2'}`
+aws sns delete-topic --topic-arn $TOPICARN   
