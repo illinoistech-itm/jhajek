24df098d829ca9cf70d1801a3e22ccff316c3ee7
Jeremy R Hajek <hajek@iit.edu>
2019-11-19 23:29:55 -0600

    added perens

--- itmo-444/Week-14/get-item-all-fields-dynamodb.php
+++ itmo-444/Week-14/get-item-all-fields-dynamodb.php
@@ -23,7 +23,7 @@ $result = $client->getItem([
 print_r($result);
 
 echo "Results: " . "\n";
-print_r $result['Item'];
+print_r($result['Item']);
 echo "Raw: " . $result['S3rawurl'];
 
 /* ProjectionExpression - A string that identifies one or more attributes to retrieve from the table. 
