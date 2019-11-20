c03b2fcb6aa94c4b149cd8f70d33cf3c02d0f2fe
Jeremy R Hajek <hajek@iit.edu>
2019-11-20 09:35:33 -0600

    updated SNS sample

--- itmo-444/Week-14/php/sns-messages-send-recv.php
+++ itmo-444/Week-14/php/sns-messages-send-recv.php
@@ -12,22 +12,42 @@ $client = new SnsClient([
 ]);
 
 # list topic ARN
+# https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-sns-2010-03-31.html#listtopics
+
+$result = $client->listTopics([
+   // no need to call anything, as it will list all
+]);
+
+print_r($result);
+
+$TopicArn = $result['Topics']['TopicArn'];
+echo "\n" . $TopicArn . "\n";
+
 
 # https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-sns-2010-03-31.html#subscribe
 $result = $client->subscribe([
     'Endpoint' => '16306389708',  // this number is taken from the form on index.php POST action
     'Protocol' => sms, // REQUIRED
-    'ReturnSubscriptionArn' => true || false,
-    'TopicArn' => '<string>', // REQUIRED
+    'ReturnSubscriptionArn' => true,
+    'TopicArn' => $TopicArn, // REQUIRED
 ]);
 
 
 # https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-sns-2010-03-31.html#confirmsubscription
+/*
 $result = $client->confirmSubscription([
     'AuthenticateOnUnsubscribe' => '<string>',
     'Token' => '<string>', // REQUIRED  sent during subscribe
     'TopicArn' => '<string>', // REQUIRED
 ]);
+*/
 
+# https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-sns-2010-03-31.html#publish
+$result = $client->publish([
+    'Message' => 'Your image is ready - add gallery URL here', // REQUIRED
+    'PhoneNumber' => '16306389708',  // Use your own
+    'Subject' => 'Submitted Image ready',
+    'TopicArn' => '$TopicArn,
+]);
 
 ?>
