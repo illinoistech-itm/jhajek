88d840f72f7cfd700d6297b94ce075742680be61
Jeremy R Hajek <hajek@iit.edu>
2019-11-20 00:42:25 -0600

    added links

--- itmo-444/Week-14/install-python-sdk-for-rendering.sh
+++ itmo-444/Week-14/install-python-sdk-for-rendering.sh
@@ -8,3 +8,7 @@ pip3 install Pillow
 
 # Install PIL  - python image library
 # https://docs.python-guide.org/scenarios/imaging/
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/examples.html
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-example-download-file.html
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/sqs.html
