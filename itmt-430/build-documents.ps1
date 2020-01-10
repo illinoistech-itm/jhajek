d3ea8c4cd7d297c31e61f843fb2d7dd3b3b2dc90
Jeremy R Hajek <hajek@iit.edu>
2020-01-10 14:26:42 -0600

    Updated documents

---
+++ itmt-430/build-documents.ps1
@@ -0,0 +1,8 @@
+pandoc -s -o ./student-book-presentation/student-book-presentation.docx ./student-book-presentation/Readme.md
+pandoc -s -o ./student-book-presentation/student-book-presentation.txt ./student-book-presentation/Readme.md
+pandoc -s -o ./Introduction-concepts-presentations/Introduction-concepts-presentations.txt ./Introduction-concepts-presentations/Readme.md
+pandoc -s -o ./Introduction-concepts-presentations/Introduction-concepts-presentations.docx ./Introduction-concepts-presentations/Readme.md
+pandoc -s -o ./sprint-dates/sprint-dates.txt ./sprint-dates/Readme.md
+pandoc -s -o ./sprint-dates/sprint-dates.docx ./sprint-dates/Readme.md
+pandoc -s -o ./tooling-assignments/tooling-assignments.txt ./tooling-assignments/Readme.md
+pandoc -s -o ./tooling-assignments/tooling-assignments.docx ./tooling-assignments/Readme.md
