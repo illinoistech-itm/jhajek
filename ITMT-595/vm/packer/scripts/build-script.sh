288fda3c04f539fe65bdf88e10422b8f6e4d132a
Jeremy R Hajek <hajek@iit.edu>
2020-03-10 17:18:25 -0500

    moved to induvidual build scripts

--- ITMT-595/vm/packer/scripts/build-script.sh
+++ ITMT-595/vm/packer/scripts/build-script.sh
@@ -6,5 +6,10 @@ bash ./remove-vms.sh
 # Build each Server
 cd ../vanilla-install/ || exit 1
 
-packer build -force all-servers.json || exit 1
+#packer build -force all-servers.json || exit 1
+packer build mongodb-server.json
+packer build nginx-web-server.json
+packer build node-application-server.json
+packer redis-caching-server.json
+
 echo "[PACKER] build finished..."
