1cb9752c601038c503872351c2013ea0afb48857
Jeremy R. Hajek <hajek@iit.edu>
2020-03-14 23:41:15 -0500

    Added variables json

--- ITMT-595/vm/packer-build-scripts/vanilla-install/build-everything.ps1
+++ ITMT-595/vm/packer-build-scripts/vanilla-install/build-everything.ps1
@@ -6,9 +6,9 @@ vagrant plugin install vagrant-vbguest
 #bash add-all-vms.sh || exit 1
 #bash start-vms-script.sh
 
-packer build mongodb-server.json
-packer build nginx-web-server.json
-packer build node-application-server.json
-packer build redis-caching-server.json
+packer build --var-files=./variables.json mongodb-server.json
+packer build --var-files=./variables.json nginx-web-server.json
+packer build --var-files=./variables.json node-application-server.json
+packer build --var-files=./variables.json redis-caching-server.json
 
 echo "[PACKER] build finished..."
