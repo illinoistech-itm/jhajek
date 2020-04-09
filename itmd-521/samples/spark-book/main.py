be7833002153ab41f191e606e1e9c31bf96d3fbf
Jeremy R Hajek <hajek@iit.edu>
2020-04-08 22:32:36 -0500

    added main.py

---
+++ itmd-521/samples/spark-book/main.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+
+    from pyspark.sql import SparkSession
+
+    spark = SparkSession.builder \
+        .master("local") \
+        .appName("Word Count") \
+        .getOrCreate()
+
+    print(spark.range(5000).where("id > 500").selectExpr("sum(id)").collect())
