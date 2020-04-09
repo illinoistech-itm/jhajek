4da32a4a065516fc7d269acb8717fd2d7c518832
Jeremy R Hajek <hajek@iit.edu>
2020-04-09 10:49:23 -0500

    added spark context

--- itmd-521/samples/spark-book/spark-code/demo-read-fourth/main.py
+++ itmd-521/samples/spark-book/spark-code/demo-read-fourth/main.py
@@ -2,8 +2,10 @@
 from __future__ import print_function
 if __name__ == 'main':
     from pyspark.sql import SparkSession
-    spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").master("yarn").getOrCreate()
+    spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrCreate()
     
-    df = spark.read.csv("hdfs://192.168.1.100/user/controller/ncdc-parsed-csv/20/part-r-00000").option("inferSchema","true").option("header","true")
+    sc = spark.sparkContext
+
+    df = sc.read.csv("hdfs://192.168.1.100/user/controller/ncdc-parsed-csv/20/part-r-00000").option("inferSchema","true").option("header","true")
     print(df.show(10))
     
