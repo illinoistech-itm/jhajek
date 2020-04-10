c47187e67b20b39034beaa36bdd9ff1fdf2e8f3d
Jeremy R Hajek <hajek@iit.edu>
2020-04-09 23:46:58 -0500

    fixed read - is it indents causing the error?

--- itmd-521/samples/spark-book/spark-code/demo-read-fifth/demo-read.py
+++ itmd-521/samples/spark-book/spark-code/demo-read-fifth/demo-read.py
@@ -5,6 +5,6 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrCreate()
  
 df = spark.read.format("csv").option("inferSchema","true").option("header","true").load("hdfs://192.168.1.100/user/controller/ncdc-parsed-csv/20/part-r-00000")
-#df = sc.read.csv("hdfs://192.168.1.100/user/controller/ncdc-parsed-csv/20/part-r-00000").option("inferSchema","true").option("header","true")
-#print(sc.parallelize(df.show(10)))
+#df = spark.read.csv("hdfs://192.168.1.100/user/controller/ncdc-parsed-csv/20/part-r-00000").option("inferSchema","true").option("header","true")
+print(df.show(10))
     
