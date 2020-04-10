63900d70aebe98035a58ed6cfa30ba8c7794e885
Jeremy R Hajek <hajek@iit.edu>
2020-04-09 23:44:52 -0500

    updated demo-read

--- itmd-521/samples/spark-book/spark-code/demo-read-fifth/demo-read.py
+++ itmd-521/samples/spark-book/spark-code/demo-read-fifth/demo-read.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrCreate()
  
-#df = sc.read.format("csv").option("inferSchema","true").option("header","true").load("hdfs://192.168.1.100/user/controller/ncdc-parsed-csv/20/part-r-00000")
+df = spark.read.format("csv").option("inferSchema","true").option("header","true").load("hdfs://192.168.1.100/user/controller/ncdc-parsed-csv/20/part-r-00000")
 #df = sc.read.csv("hdfs://192.168.1.100/user/controller/ncdc-parsed-csv/20/part-r-00000").option("inferSchema","true").option("header","true")
 #print(sc.parallelize(df.show(10)))
     
