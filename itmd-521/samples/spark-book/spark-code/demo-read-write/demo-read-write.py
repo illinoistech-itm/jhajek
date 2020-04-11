2f4e2d380c33ea834ca57f2d7b2e77c6e039aabb
Jeremy R Hajek <hajek@iit.edu>
2020-04-10 22:13:36 -0500

    updated controller to vagrant

--- itmd-521/samples/spark-book/spark-code/demo-read-write/demo-read-write.py
+++ itmd-521/samples/spark-book/spark-code/demo-read-write/demo-read-write.py
@@ -8,4 +8,4 @@ df = spark.read.format("csv").option("inferSchema","true").option("header","true
 #df = spark.read.csv("hdfs://192.168.1.100/user/controller/ncdc-parsed-csv/20/part-r-00000").option("inferSchema","true").option("header","true")
 print(df.show(10))
 
-df.write.format("parquet").mode("overwrite").save("hdfs://namenode/user/controller/output/jrh/20-show-10.parquet")
+df.write.format("parquet").mode("overwrite").save("hdfs://namenode/user/vagrant/output/jrh/20-show-10.parquet")
