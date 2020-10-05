c983d5ede9f45870f0d4cc72e1653e40fbb28fc9
Jeremy R Hajek <hajek@iit.edu>
2020-10-05 12:39:29 -0500

    added tests

--- itmo-544-444/week-07/scripts/post_install_vagrant-EC2.sh
+++ itmo-544-444/week-07/scripts/post_install_vagrant-EC2.sh
@@ -10,4 +10,7 @@ sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_
 sudo systemctl enable ssh.service
 sudo systemctl start ssh.service
 
-sudo apt-get install -y git rsync wget links
+sudo apt-get install -y git rsync wget links apache2
+
+sudo systemctl enable apache2
+sudo systemctl start apache2
