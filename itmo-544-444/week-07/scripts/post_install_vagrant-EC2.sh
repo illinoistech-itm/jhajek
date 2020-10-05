cf8e602a7e36e08fa341dc761128ac6d76496e93
Jeremy R Hajek <hajek@iit.edu>
2020-10-05 13:21:13 -0500

    add cloud-init

--- itmo-544-444/week-07/scripts/post_install_vagrant-EC2.sh
+++ itmo-544-444/week-07/scripts/post_install_vagrant-EC2.sh
@@ -10,7 +10,10 @@ sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_
 sudo systemctl enable ssh.service
 sudo systemctl start ssh.service
 
-sudo apt-get install -y git rsync wget links apache2
+sudo apt-get install -y git rsync wget links apache2 cloud-init
+
+sudo systemctl enable cloud-init
+sudo systemctl start cloud-init
 
 sudo systemctl enable apache2
 sudo systemctl start apache2
