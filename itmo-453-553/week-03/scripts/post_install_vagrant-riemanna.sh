a5d1a301cf0314cd8850bef1bdbae872fff069be
Jeremy R Hajek <hajek@iit.edu>
2020-10-07 11:07:05 -0500

    updated samples

--- itmo-453-553/week-03/scripts/post_install_vagrant-riemanna.sh
+++ itmo-453-553/week-03/scripts/post_install_vagrant-riemanna.sh
@@ -20,7 +20,26 @@ echo "All Done!"
 ##################################################
 # Add User customizations below here
 ##################################################
+##################################################
+# Change hostname and /etc/hosts
+##################################################
+cat << EOT >> /etc/hosts
+# Nodes
+192.168.33.110 riemanna riemanna.example.com
+192.168.33.120 riemannb riemannb.example.com
+192.168.33.100 riemannmc riemannmc.example.com
+192.168.33.210 graphitea graphitea.example.com
+192.168.33.220 graphiteb graphiteb.example.com
+192.168.33.200 graphitemc graphitemc.example.com
+192.168.33.150 ela1 ela1.example.com
+192.168.33.160 ela2 ela2.example.com
+192.168.33.170 ela3 ela3.example.com
+192.168.33.180 logstash logstash.example.com
+192.168.33.10 host1 host1.example.com
+192.168.33.11 host2 host2.example.com
+EOT
 
+sudo hostnamectl set-hostname riemanna
 # Install pre-reqs
 #  Install openjdk 8
 # wget riemann 
