c0251ae735308ced8d7e660bec98db88e52d474a
Jeremy R Hajek <hajek@iit.edu>
2020-10-07 12:54:25 -0500

    updated sample code

--- itmo-544-444/week-07/scripts/post_install_vagrant-EC2.sh
+++ itmo-544-444/week-07/scripts/post_install_vagrant-EC2.sh
@@ -12,6 +12,10 @@ sudo systemctl start ssh.service
 
 sudo apt-get install -y git rsync wget links apache2 cloud-init
 
+# This changes the ownership of the private key and config file to make sure there are no permission issues
+sudo chown ubuntu:ubuntu ~/.ssh/config
+sudo chown ubuntu:ubuntu ~/.ssh/id_rsa_github_deploy_key
+
 sudo systemctl enable cloud-init
 #sudo systemctl start cloud-init
 
