b243bfa327b56a5379751770d4917f9cd16d0010
Jeremy R Hajek <hajek@iit.edu>
2020-10-20 21:48:15 -0500

    updated running scripts

---
+++ itmo-453-553/sample-code/vagrant-scripts/itmo-453-553/up-ubuntu-hosts.ps1
@@ -0,0 +1,11 @@
+Set-Location ub-graphitea
+vagrant up
+Set-Location ../ub-graphitemc
+vagrant up
+Set-Location ../ub-riemanna
+vagrant up
+Set-Location ../ub-riemannmc
+vagrant up
+Set-Location ../host1
+vagrant up
+Set-Location ../
