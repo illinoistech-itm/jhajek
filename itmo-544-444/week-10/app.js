28efec2701fbd1089ab1c7acdb6ca25f86d831a8
Jeremy R Hajek <hajek@iit.edu>
2020-11-04 14:54:04 -0600

    added endpoint

--- itmo-544-444/week-10/app.js
+++ itmo-544-444/week-10/app.js
@@ -15,6 +15,8 @@ aws.config.update({
 var app = express(),
     s3 = new aws.S3();
 
+ var endpoint = s3.Endpoint();
+
 app.use(bodyParser.json());
 
 var upload = multer({
@@ -73,7 +75,7 @@ connection.query(
 
   s3.listObjects(paramss3, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data.Contents[0]);           // successful response
+    else      console.log(data.Contents[0]['key']); console.log(endpoint);           // successful response
   });
 
         res.write(dbhost);
