d76f03e55d0219affe363676cd4e513406b5d5c9
Jeremy R Hajek <hajek@iit.edu>
2020-11-04 13:52:48 -0600

    added paramsS3

--- itmo-544-444/week-10/app.js
+++ itmo-544-444/week-10/app.js
@@ -69,6 +69,12 @@ connection.query(
       console.log(results); // results contains rows returned by server
      }
   );
+
+  s3.listObjects(paramss3, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+
         res.write(dbhost);
         res.write("<br />File uploaded successfully to Amazon S3 Server!<br />");
 
