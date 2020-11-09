673251586135bee0ca8d7871126589d5e514eff8
Jeremy R Hajek <hajek@iit.edu>
2020-11-09 14:47:13 -0600

    added sample boto links

--- itmo-544-444/week-10/python/retreive-message-sqs.py
+++ itmo-544-444/week-10/python/retreive-message-sqs.py
@@ -17,5 +17,17 @@ for message in queue.receive_messages(MessageAttributeNames=['Author']):
     # Print out the body and author (if set)
     print('Hello, {0}!{1}'.format(message.body, author_text))
 
-    # Let the queue know that the message is processed
-    message.delete()
+### Connect to Mysql Database and retrieve record relating to the SQS job
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds.html
+#  SELECT * FROM jobs WHERE id = message.body;
+
+# Retreive image from the S3 bucket
+
+# Process image with PIL 
+
+# Put Image Object back into S3 Bucket
+
+ # SQL UPDATE record where id == message.body  set Stat from 0 to 1
+
+# Let the queue know that the message is processed
+message.delete()
