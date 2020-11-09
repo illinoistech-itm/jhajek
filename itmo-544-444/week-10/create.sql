57583d32108f72105d944f0315092491191ac8c0
Jeremy R Hajek <hajek@iit.edu>
2020-11-08 23:03:00 -0600

    updated schema

--- itmo-544-444/week-10/create.sql
+++ itmo-544-444/week-10/create.sql
@@ -5,12 +5,12 @@ USE company;
 CREATE TABLE jobs
 (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-RecordNumber VARCHAR(32), -- This is the UUID
-CustomerName VARCHAR(32),
-Email VARCHAR(32),
-Phone VARCHAR(12),
-Stat INT(1) DEFAULT 0, -- Job status, not done is 0, done is 1
-S3URL VARCHAR(100) -- set the returned S3URL here
+RecordNumber VARCHAR(64) NOT NULL, -- This is the UUID
+CustomerName VARCHAR(64) NOT NULL,
+Email VARCHAR(64) NOT NULL,
+Phone VARCHAR(32) NOT NULL,
+Stat INT(1) NOT NULL DEFAULT 0, -- Job status, not done is 0, done is 1
+S3URL VARCHAR(200) NOT NULL, -- set the returned S3URL here
 );
 
 INSERT INTO jobs(RecordNumber,CustomerName,Email,Phone,Stat,S3URL) VALUES('00000',"NAME","email@iit.edu","000-000-0000",0,"http://");
