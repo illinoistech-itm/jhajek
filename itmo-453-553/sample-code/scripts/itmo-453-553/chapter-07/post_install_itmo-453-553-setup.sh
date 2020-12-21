927d01537b69325ee8f0740b06a3bc33c53574cc
Jeremy R Hajek <hajek@iit.edu>
2020-12-20 22:24:03 -0600

    updated post installs script to configure collectd

--- itmo-453-553/sample-code/scripts/itmo-453-553/chapter-07/post_install_itmo-453-553-setup.sh
+++ itmo-453-553/sample-code/scripts/itmo-453-553/chapter-07/post_install_itmo-453-553-setup.sh
@@ -54,3 +54,26 @@ sudo dpkg -i docker-ce_19.03.9~3-0~ubuntu-bionic_amd64.deb
 sudo dpkg -i docker-ce-cli_19.03.9~3-0~ubuntu-bionic_amd64.deb
 
 sudo systemctl enable docker
+
+git clone https://github.com/turnbullpress/docker-collectd-plugin.git
+
+python3 -m pip install -r ~/docker-collectd-plugin/requirements.txt
+
+##################################################
+sudo apt-get update -y
+sudo apt-get install -y collectd stress
+
+# Cloning source code examples for the book
+git clone https://github.com/turnbullpress/aom-code.git
+
+# Collectd config filees
+sudo cp -v /home/vagrant/aom-code/7/collectd/collectd.conf /etc
+sudo sudo cp -rv /home/vagrant/aom-code/7/collectd/collectd.d/*.conf /etc/collectd/collectd.conf.d
+# sudo sed -i 's/riemanna.exmaple.com/riemanna.example.com/g' /etc/collectd.d/write_riemann.conf
+sudo sed -i 's/Node "riemanna"/Node "docker1"/g' /etc/collectd/collectd.conf.d/write_riemann.conf
+
+# Reload collectd service and start it at boot
+sudo systemctl enable collectd
+sudo systemctl stop collectd
+sudo systemctl daemon-reload
+sudo systemctl start collectd
