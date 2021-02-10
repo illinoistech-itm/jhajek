c0e5d7817709802ec915d6fbf6f218b9d0ddecce
Jeremy R Hajek <hajek@iit.edu>
2021-02-09 21:52:10 -0600

    added conditional test

--- itmt-495-595/professors-project-code/creation-scripts/delete-original-box-files.ps1
+++ itmt-495-595/professors-project-code/creation-scripts/delete-original-box-files.ps1
@@ -1,5 +1,10 @@
 Set-Location -path ../../build
 
-Remove-Item -Verbose *.box
+if (Test-Path -Path ./*.box -PathType Leaf) {
+    Remove-Item -Verbose *.box
+}
+else {
+    Write-Host "No .box files to delete"
+}
 
-Set-Location -path ../../professors-project-code/creation-scripts
+Set-Location -path ../professors-project-code/creation-scripts
