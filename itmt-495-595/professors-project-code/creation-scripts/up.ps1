8909fcabcfedacecacaedf918893797612120402
Jeremy R Hajek <hajek@iit.edu>
2021-02-23 22:04:24 -0600

    changed start order

--- itmt-495-595/professors-project-code/creation-scripts/up.ps1
+++ itmt-495-595/professors-project-code/creation-scripts/up.ps1
@@ -2,6 +2,15 @@
 
 Set-Location -path ../../build
 
+# Change location to the ws3 directory
+Set-Location -path ../mm
+vagrant up
+# Change location to the ws3 directory
+Set-Location -path ../ms1
+vagrant up
+# Change location to the ws3 directory
+Set-Location -path ../ms2
+vagrant up
 # Change location to the lb directory
 Set-Location -path ./lb
 vagrant up
@@ -14,14 +23,6 @@ vagrant up
 # Change location to the ws3 directory
 Set-Location -path ../ws3
 vagrant up
-# Change location to the ws3 directory
-Set-Location -path ../mm
-vagrant up
-# Change location to the ws3 directory
-Set-Location -path ../ms1
-vagrant up
-# Change location to the ws3 directory
-Set-Location -path ../ms2
-vagrant up
+
 
 Set-Location -path ../../professors-project-code/creation-scripts
