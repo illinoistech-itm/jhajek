17a999e703fc95f9d7f46b2d997cb7520e465ee7
Jeremy R Hajek <hajek@iit.edu>
2021-03-09 12:36:07 -0600

    updated the start process for mongodb

--- itmt-495-595/scripts/post_install_vagrant-fs-mm.sh
+++ itmt-495-595/scripts/post_install_vagrant-fs-mm.sh
@@ -136,7 +136,9 @@ wget https://downloads.mongodb.com/compass/mongosh_0.8.0_amd64.deb
 sudo dpkg -i mongosh_0.8.0_amd64.deb
 
 # copy the new mongodb.conf over the old one
-sudo cp -v ~/hajek/itmt-595/fullstack-app-code/mongodb/m1/mongod.conf /etc/
+sudo cp -v ~/hajek/itmt-595/fullstack-app-code/mongodb/m3/mongod.conf /etc/
 sudo systemctl start mongod.service
 sudo systemctl enable mongod.service
+sudo systemctl daemon-reload
+sudo systemctl restart mongod.service
 sudo systemctl status mongod.service
