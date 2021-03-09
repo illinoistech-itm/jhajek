d35fd2c61f34bf79d5a8288d750abd9faa74199b
Jeremy R Hajek <hajek@iit.edu>
2021-03-09 15:28:23 -0600

    updated delete remove

--- itmt-495-595/professors-project-code/creation-scripts/delete-remove-vagrant-boxes.ps1
+++ itmt-495-595/professors-project-code/creation-scripts/delete-remove-vagrant-boxes.ps1
@@ -1,43 +1,5 @@
 # This script will remove the Vagrant boxes (delete) and delete the built artifacts
 
-# Destroy existing vagrant boxes
-Set-Location ../../build
-
-Set-Location ./lb
-vagrant halt
-vagrant destroy -f
-Remove-Item -Verbose -Force ./.vagrant -Recurse
-
-Set-Location ../ws1
-vagrant halt
-vagrant destroy -f
-Remove-Item -Verbose -Force ./.vagrant -Recurse
-
-Set-Location ../ws2
-vagrant halt
-vagrant destroy -f
-Remove-Item -Verbose -Force ./.vagrant -Recurse
-
-Set-Location ../ws3
-vagrant halt
-vagrant destroy -f
-Remove-Item -Verbose -Force ./.vagrant -Recurse
-
-Set-Location ../mm
-vagrant halt
-vagrant destroy -f
-Remove-Item -Verbose -Force ./.vagrant -Recurse
-
-Set-Location ../ms1
-vagrant halt
-vagrant destroy -f
-Remove-Item -Verbose -Force ./.vagrant -Recurse
-
-Set-Location ../ms2
-vagrant halt
-vagrant destroy -f
-Remove-Item -Verbose -Force ./.vagrant -Recurse
-
 # Remove existing vagrant boxes
 vagrant box remove lb --force 
 vagrant box remove ws1 --force
@@ -46,5 +8,29 @@ vagrant box remove ws3 --force
 vagrant box remove mm --force
 vagrant box remove ms1 --force 
 vagrant box remove ms2 --force 
+
+# Destroy existing vagrant boxes
+Set-Location ../../build
+
+Set-Location ./lb
+Remove-Item -Verbose -Force ./.vagrant -Recurse
+
+Set-Location ../ws1
+Remove-Item -Verbose -Force ./.vagrant -Recurse
+
+Set-Location ../ws2
+Remove-Item -Verbose -Force ./.vagrant -Recurse
+
+Set-Location ../ws3
+Remove-Item -Verbose -Force ./.vagrant -Recurse
+
+Set-Location ../mm
+Remove-Item -Verbose -Force ./.vagrant -Recurse
+
+Set-Location ../ms1
+Remove-Item -Verbose -Force ./.vagrant -Recurse
+
+Set-Location ../ms2
+Remove-Item -Verbose -Force ./.vagrant -Recurse
 
 Set-Location -path ../../professors-project-code/creation-scripts
