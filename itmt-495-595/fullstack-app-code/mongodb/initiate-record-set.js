588ec64e83f005f07c8a3bc3080b1ad85be26aa8
Jeremy R Hajek <hajek@iit.edu>
2021-03-10 11:27:31 -0600

    fixed ips

--- itmt-495-595/fullstack-app-code/mongodb/initiate-record-set.js
+++ itmt-495-595/fullstack-app-code/mongodb/initiate-record-set.js
@@ -1,1 +1,1 @@
-rs.initiate({_id : "rs0",members: [{ _id: 0, host: "mm.class.edu:27017" },{ _id: 1, host: "ms1.class.edu:27017" },{ _id: 2, host: "ms2.class.edu:27017" }]});
+rs.initiate({_id : "rs0",members: [{ _id: 0, host: "mm:27017" },{ _id: 1, host: "ms1:27017" },{ _id: 2, host: "ms2:27017" }]});
