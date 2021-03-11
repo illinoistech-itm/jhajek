a438821ca59fdf9209d9f841473bad1db405f523
Jeremy R Hajek <hajek@iit.edu>
2021-03-10 23:03:02 -0600

    updated priority

--- itmt-495-595/fullstack-app-code/mongodb/initiate-record-set.js
+++ itmt-495-595/fullstack-app-code/mongodb/initiate-record-set.js
@@ -1,1 +1,1 @@
-rs.initiate({_id : "rs0",members: [{ _id: 0, host: "mm:27017", priority: 2},{ _id: 1, host: "ms1:27017", priority: 0.5 },{ _id: 2, host: "ms2:27017", priority: 0.5 }]});
+rs.initiate({_id : "rs0",members: [{ _id: 0, host: "mm:27017", priority: 10},{ _id: 1, host: "ms1:27017", priority: 0.5 },{ _id: 2, host: "ms2:27017", priority: 0.5 }]});
