21161474072ed8c6ee1eb38ec4a517512b915a29
Jeremy R Hajek <hajek@iit.edu>
2021-03-23 14:35:44 -0500

    made ajustements to settings.py

--- itmt-430/sample-team-repo-structure/sprint-03/code/django/settings.py
+++ itmt-430/sample-team-repo-structure/sprint-03/code/django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.33.34']
+ALLOWED_HOSTS = []
 
 
 # Application definition
