01d84114e343167eefacbe45481d58d282ea8597
Jeremy R Hajek <hajek@iit.edu>
2021-03-25 21:30:56 -0500

    removed apache install

--- itmt-430/sample-team-repo-structure/sprint-03/build-scripts/provisioner-scripts/post_install_django-install-and-configure-webserver.sh
+++ itmt-430/sample-team-repo-structure/sprint-03/build-scripts/provisioner-scripts/post_install_django-install-and-configure-webserver.sh
@@ -8,7 +8,7 @@ echo "$DATABASESERVERIP    backend    backend.class.edu"    | sudo tee -a /etc/h
 ##############################################################################################
 # Install Django pre-reqs
 ##############################################################################################
-sudo apt-get install -y apache2 libexpat1 apache2-utils ssl-cert libapache2-mod-wsgi python3-dev python3-pip python3-setuptools
+sudo apt-get install -y libexpat1 ssl-cert python3-dev python3-pip python3-setuptools
 
 ##############################################################################################
 # Install Django mysqlclient library pre-reqs
