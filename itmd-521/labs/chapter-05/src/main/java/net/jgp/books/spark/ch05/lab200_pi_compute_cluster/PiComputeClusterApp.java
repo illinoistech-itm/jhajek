ebdf6c0c8fb6f74fcac0c53995f425429c38c143
Jeremy R Hajek <hajek@iit.edu>
2021-04-07 11:28:54 -0500

    changed executor memory to 8g

--- itmd-521/labs/chapter-05/src/main/java/net/jgp/books/spark/ch05/lab200_pi_compute_cluster/PiComputeClusterApp.java
+++ itmd-521/labs/chapter-05/src/main/java/net/jgp/books/spark/ch05/lab200_pi_compute_cluster/PiComputeClusterApp.java
@@ -82,7 +82,7 @@ public class PiComputeClusterApp implements Serializable {
         .appName("JavaSparkPi on a cluster - jrh")
         //.master("spark://un:7077")
         .master("spark://172.16.1.23:7077")
-        .config("spark.executor.memory", "4g")
+        .config("spark.executor.memory", "8g")
         // Uncomment the next block if you want to run your application from
         // the IDE - note that you will have to deploy the jar first to
         // *every* worker. Spark can share a jar from which it is launched -
