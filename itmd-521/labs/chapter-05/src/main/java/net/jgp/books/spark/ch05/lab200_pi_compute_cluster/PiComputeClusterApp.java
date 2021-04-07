70f37dccfd4bc4f73080e86a550bcedeb7209d90
Jeremy R Hajek <hajek@iit.edu>
2021-04-07 12:24:15 -0500

    updated deploy mode

--- itmd-521/labs/chapter-05/src/main/java/net/jgp/books/spark/ch05/lab200_pi_compute_cluster/PiComputeClusterApp.java
+++ itmd-521/labs/chapter-05/src/main/java/net/jgp/books/spark/ch05/lab200_pi_compute_cluster/PiComputeClusterApp.java
@@ -84,6 +84,7 @@ public class PiComputeClusterApp implements Serializable {
         .master("spark://172.16.1.23:7077")
 //        .config("spark.executor.memory", "1g")
         .config("spark.executor.cores", 1)
+        .config("spark.submit.deployMode", "cluster")
         // Uncomment the next block if you want to run your application from
         // the IDE - note that you will have to deploy the jar first to
         // *every* worker. Spark can share a jar from which it is launched -
