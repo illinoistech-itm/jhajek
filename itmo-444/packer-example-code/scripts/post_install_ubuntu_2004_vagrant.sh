11756bdaa9de24a133b7d9971049c79be7b2411b
Jeremy R Hajek <hajek@iit.edu>
2021-09-02 10:54:40 -0500

    update: fixed correct user permission

--- itmo-444/packer-example-code/scripts/post_install_ubuntu_2004_vagrant.sh
+++ itmo-444/packer-example-code/scripts/post_install_ubuntu_2004_vagrant.sh
@@ -7,14 +7,14 @@ set -v
 # This line assumes the user you created in the preseed directory is ubuntu
 echo "%admin  ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/init-users
 sudo groupadd admin
-sudo usermod -a -G admin ubuntu
+sudo usermod -a -G admin vagrant
 
 # Installing Vagrant keys
 wget --no-check-certificate 'https://raw.githubusercontent.com/hashicorp/vagrant/main/keys/vagrant.pub'
-sudo mkdir -p /home/ubuntu/.ssh
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.ssh
-cat ./vagrant.pub >> /home/ubuntu/.ssh/authorized_keys
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys
+sudo mkdir -p /home/vagrant/.ssh
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh
+cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh/authorized_keys
 echo "All Done!"
 
 ##################################################
