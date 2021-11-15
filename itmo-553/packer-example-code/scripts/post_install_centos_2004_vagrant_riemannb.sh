9aa0c131059ef3a61023e2f2c1ae1e57b4afd1c5
Jeremy R Hajek <hajek@iit.edu>
2021-11-14 21:18:53 -0600

    update: added collectd install

--- itmo-553/packer-example-code/scripts/post_install_centos_2004_vagrant_riemannb.sh
+++ itmo-553/packer-example-code/scripts/post_install_centos_2004_vagrant_riemannb.sh
@@ -32,7 +32,7 @@ echo "All Done!"
 # Add customization here
 #########################
 
-sudo yum install -y kernel-devel-`uname -r` gcc binutils make perl bzip2
+sudo yum install -y kernel-devel-`uname -r` gcc binutils make perl bzip2 epel-release
 
 echo "All Done!"
 
@@ -59,14 +59,35 @@ sudo yum install -y java-1.8.0-openjdk ruby ruby-devel
 wget https://github.com/riemann/riemann/releases/download/0.3.6/riemann-0.3.6-1.noarch-EL8.rpm
 sudo rpm -i riemann-0.3.6-1.noarch-EL8.rpm
 # 3 we will need some ruby gems 
-sudo gem install riemann-client riemann-tools riemann-dash 
+sudo gem install riemann-client riemann-tools
 # 4 We need to ensure the services are enabled and start succesfully
 sudo systemctl enable riemann
 sudo systemctl start riemann
 
 git clone git@github.com:illinoistech-itm/sample-student.git
-cp -v sample-student/itmo-453/week-07/riemann/riemannb/riemann.config /etc/riemann/riemann.conf
+cp -v sample-student/itmo-453/week-07/riemann/riemannb/riemann.config /etc/riemann/riemann.config
 
 sudo systemctl stop riemann
 sudo systemctl start riemann
 
+##################################################
+# Installation and cofiguration of collectd
+##################################################
+sudo yum install -y collectd
+
+sudo systemctl stop collectd
+
+#####################################################
+# Copy the collectd configuration files from week-12
+#####################################################
+cp -v sample-student/itmo-453/week-12/riemanna/collectd.conf.d/* /etc/collectd.conf.d/
+
+cp -v sample-student/itmo-453/week-12/collectd.conf /etc/
+
+sudo systemctl daemon-reload
+sudo systemctl start collectd
+
+#######################################################
+# Using sed to find and replace riemanna in the write_riemann.conf collectd conf file
+#######################################################
+sed -i 's/"riemanna"/"riemannb"/' write_riemann.conf
--- itmo-553/packer-example-code/scripts/post_install_ubuntu_2004_vagrant_riemannmc.sh
+++ itmo-553/packer-example-code/scripts/post_install_ubuntu_2004_vagrant_riemannmc.sh
@@ -45,15 +45,35 @@ sudo apt-get install -y openjdk-8-jre ruby ruby-dev
 wget https://github.com/riemann/riemann/releases/download/0.3.6/riemann_0.3.6_all.deb
 sudo dpkg -i riemann_0.3.6_all.deb
 # 3 we will need some ruby gems 
-sudo gem install riemann-client riemann-tools riemann-dash 
+sudo gem install riemann-client riemann-tools
 # 4 We need to ensure the services are enabled and start succesfully
 sudo systemctl enable riemann
 sudo systemctl start riemann
 
 git clone git@github.com:illinoistech-itm/sample-student.git
-cp -v sample-student/itmo-453/week-07/riemann/riemannmc/riemann.config /etc/riemann/riemann.conf
+cp -v sample-student/itmo-453/week-07/riemann/riemannmc/riemann.config /etc/riemann/riemann.config
 
 sudo systemctl stop riemann
 sudo systemctl start riemann
 
-rm ~/.ssh/id_rsa*
+##################################################
+# Installation and cofiguration of collectd
+##################################################
+sudo apt-get update -y
+sudo apt-get install -y collectd
+sudo systemctl stop collectd
+
+#####################################################
+# Copy the collectd configuration files from week-12
+#####################################################
+cp -v sample-student/itmo-453/week-12/riemanna/collectd.conf.d/* /etc/collectd/collectd.conf.d/
+
+cp -v sample-student/itmo-453/week-12/collectd.conf /etc/collectd/
+
+sudo systemctl daemon-reload
+sudo systemctl start collectd
+
+#######################################################
+# Using sed to find and replace riemanna in the write_riemann.conf collectd conf file
+#######################################################
+sed -i 's/"riemanna"/"riemannb"/' write_riemann.conf
