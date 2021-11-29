89ee60a8d24e6ab8be454e49fcc766f60f202fac
Jeremy R Hajek <hajek@iit.edu>
2021-11-29 16:48:02 -0600

    update: sample code

--- itmo-544/mp2/create-env.sh
+++ itmo-544/mp2/create-env.sh
@@ -3,6 +3,9 @@
 # Reuse all the code from mp1 - remove the RDS content, no need for that in this project
 
 # Use the AWS CLI to Create a S3 Bucket
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3/mb.html
+# code to create S3 bucket
+aws s3 mb s3://${11}
 
 # Create DynamoDB Table
 # I am giving you the table creation script for DynamoDB
@@ -14,6 +17,7 @@ aws dynamodb create-table --table-name ${10} \
 
 # Create SNS topic (to subscribe the users phone number to)
 # Use the AWS CLI to create the SNS
+aws sns create-topic --name $9
 
 # Install ELB and EC2 instances here -- remember to add waiters and provide and --iam-instance-profile so that your EC2 instances have permission to access SNS, S3, and DynamoDB
 # Sample
