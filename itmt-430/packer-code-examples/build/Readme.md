7cd337931bf99cbb9f2db6d74bb74e725180e763
Jeremy R Hajek <hajek@iit.edu>
2022-02-14 22:14:17 -0600

    update adding EFI kernel boot

---
+++ itmt-430/packer-code-examples/build/Readme.md
@@ -0,0 +1,3 @@
+# Scripts For Automating the Creation of the App
+
+This directory will include build scripts of Bash and PowerShell in order to allow you to automate the creation, addition, and destruction of your site
---
+++ itmt-430/packer-code-examples/build/windows/halt.ps1
@@ -0,0 +1,21 @@
+#########################################################################################
+# This script will poweroff or halt all of the Vagrant boxes you have initialized via the
+# vagrant halt command
+#########################################################################################
+# Declare and array of all the box names
+$directories='lb','ws1','ws2','ws3','db'
+# Setting initial directory location
+Write-Host "Setting initial directory location: "
+Set-Location -Path ../project
+
+ForEach ($directory in $directories)
+{
+    Write-Host "Entering directory: $directory"
+    Set-Location -Path $directory
+    # Start each virtual machine
+    Write-Host "Starting vagrant box $directory"
+    vagrant halt
+    Write-Host "Finished removing all previous Vagrant elements of your application"
+    # Resetting location up one levels
+    Set-Location -Path ../
+}
