b8584f9961f85f30514d01d658da76a43dec46f2
Jeremy R Hajek <hajek@iit.edu>
2022-02-24 11:07:59 -0600

    added create permission

--- itmd-521/assignment-04/create-user.sql
+++ itmd-521/assignment-04/create-user.sql
@@ -1,3 +1,3 @@
 -- This is a SQL command that creates a non-root user for a MySQL database
 
-GRANT SELECT,INSERT,UPDATE, DELETE ON employees.* TO worker@'localhost' IDENTIFIED BY 'cluster'; flush privileges;
+GRANT SELECT,INSERT, CREATE, UPDATE, DELETE ON employees.* TO worker@'localhost' IDENTIFIED BY 'cluster'; flush privileges;
