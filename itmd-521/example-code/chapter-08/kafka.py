f6d31b046a9c828dd65d481a6659e9b3b17ef284
Jeremy R Hajek <hajek@iit.edu>
2022-03-30 23:47:40 -0500

    change 1 sec time

--- itmd-521/example-code/chapter-08/kafka.py
+++ itmd-521/example-code/chapter-08/kafka.py
@@ -15,6 +15,6 @@ resultDF = inputDF.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
 #streamingQuery = (counts.writeStream.format("console").outputMode("complete").trigger(processingTime="1 second").start())
 #streamingQuery = (inputDF.writeStream.format("kafka").option("path","./").trigger(processingTime="20 seconds").start())
 
-resultDF.writeStream.outputMode("append").format("console").trigger(processingTime="20 seconds").start()
+resultDF.writeStream.outputMode("append").format("console").trigger(processingTime="1 second").start()
 
 #streamingQuery.awaitTermination()
