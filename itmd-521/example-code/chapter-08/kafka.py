f3f722f2221bafbf3810162fd09e1eba29b068b6
Jeremy R Hajek <hajek@iit.edu>
2022-03-30 23:24:22 -0500

    update kafka

---
+++ itmd-521/example-code/chapter-08/kafka.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.streaming import *
+
+spark =  spark = (SparkSession
+        .builder
+        .appName("Kafka listener")
+        .getOrCreate())
+
+inputDF = (spark.readStream.format("kafka").option("kafka.bootstrap.servers","localhost:9092").option("subscribe","quickstart-events").load())
+
+inputDF.show(5)
+#checkpointDir =""
+
+#streamingQuery = (counts.writeStream.format("console").outputMode("complete").trigger(processingTime="1 second").start())
+#streamingQuery = (inputDF.writeStream.format("csv").option("path","./").trigger(processingTime="20 seconds").start())
+
+#streamingQuery.awaitTermination()
