dac43935b0bfc4efe58b9e25dfa3a087eb309685
Jeremy R Hajek <hajek@iit.edu>
2022-03-30 23:40:22 -0500

    update start method to retrieve

--- itmd-521/example-code/chapter-08/kafka.py
+++ itmd-521/example-code/chapter-08/kafka.py
@@ -13,6 +13,8 @@ inputDF.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
 #checkpointDir =""
 
 #streamingQuery = (counts.writeStream.format("console").outputMode("complete").trigger(processingTime="1 second").start())
-#streamingQuery = (inputDF.writeStream.format("csv").option("path","./").trigger(processingTime="20 seconds").start())
+#streamingQuery = (inputDF.writeStream.format("kafka").option("path","./").trigger(processingTime="20 seconds").start())
+
+inputDF.writeStream.outputMode("append").format("console").start()
 
 #streamingQuery.awaitTermination()
