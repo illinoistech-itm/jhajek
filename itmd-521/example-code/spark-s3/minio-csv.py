d4dc8559947d569df2fdd5aa808702447552a54c
Jeremy R Hajek <hajek@iit.edu>
2022-04-12 11:15:44 -0500

    update env variables

--- itmd-521/example-code/spark-s3/minio-csv.py
+++ itmd-521/example-code/spark-s3/minio-csv.py
@@ -3,18 +3,15 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType
 from pyspark.sql.functions import to_date
 
-# Removing hard coded password - using .env to import them
-import dotenv
-dotenv.load_dotenv()
-SECRETKEY = os.getenv('secret-key')
-ACCESSKEY = os.getenv('access-key')
+# Removing hard coded password - using os module to import them
+import os
 
 conf = SparkConf()
 conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')
 conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
  
-conf.set('spark.hadoop.fs.s3a.access.key', SECRETKEY)
-conf.set('spark.hadoop.fs.s3a.secret.key', ACCESSKEY)
+conf.set('spark.hadoop.fs.s3a.access.key', os.getenv('SECRETKEY'))
+conf.set('spark.hadoop.fs.s3a.secret.key', os.getenv('ACCESSKEY'))
 conf.set("spark.hadoop.fs.s3a.endpoint", "http://192.168.172.50:9000")
 
 spark = SparkSession.builder.appName("JRH convert 40.txt to csv").config(conf=conf).getOrCreate()
