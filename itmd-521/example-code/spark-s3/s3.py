bc0339d6c0890986212c4f308abde0f9a9cb552b
Jeremy R Hajek <hajek@iit.edu>
2022-04-11 21:47:00 -0500

    update inferschema

--- itmd-521/example-code/spark-s3/s3.py
+++ itmd-521/example-code/spark-s3/s3.py
@@ -5,11 +5,8 @@ conf = SparkConf()
 conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')
 conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
  
-conf.set('spark.hadoop.fs.s3a.access.key', spark521)
-conf.set('spark.hadoop.fs.s3a.secret.key', 79a93eda-ba02-11ec-8a4c-54ee75516ff6)
-
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
  
-df = spark.read.csv('s3a://192.168.172.50/itmd521/sf-fire-calls.csv', inferSchema=True)
+df = spark.read.csv('s3a://noaa-ghcn-pds/csv/2020.csv', inferSchema=True)
 
 df.printSchema()
