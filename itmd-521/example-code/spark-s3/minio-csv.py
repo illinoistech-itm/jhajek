af2f6b2396cbf9513795b9f9aaa07916663fc76e
Jeremy R Hajek <hajek@iit.edu>
2022-04-12 21:24:50 -0500

    update 20

--- itmd-521/example-code/spark-s3/minio-csv.py
+++ itmd-521/example-code/spark-s3/minio-csv.py
@@ -16,7 +16,7 @@ conf.set("spark.hadoop.fs.s3a.endpoint", "http://192.168.172.50:9000")
 
 spark = SparkSession.builder.appName("JRH convert 40.txt to csv").config(conf=conf).getOrCreate()
 
-df = spark.read.csv('s3a://itmd521/40.txt')
+df = spark.read.csv('s3a://itmd521/20.txt')
 
 splitDF = df.withColumn('WeatherStation', df['_c0'].substr(5, 6)) \
 .withColumn('WBAN', df['_c0'].substr(11, 5)) \
@@ -41,4 +41,4 @@ splitDF = df.withColumn('WeatherStation', df['_c0'].substr(5, 6)) \
 splitDF.printSchema()
 splitDF.show(5)
 
-splitDF.write.format("csv").mode("overwrite").option("header","true").save("s3a://itmd521/40.csv")
+splitDF.write.format("csv").mode("overwrite").option("header","true").save("s3a://itmd521/20.csv")
