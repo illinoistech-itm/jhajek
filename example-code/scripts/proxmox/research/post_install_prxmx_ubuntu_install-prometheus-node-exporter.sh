7b2e2a615014289bd071e76b92c25e6e47cecce4
Jeremy Hajek <hajek@iit.edu>
2022-11-03 11:53:39 -0500

    update adding sudo

--- example-code/scripts/proxmox/research/post_install_prxmx_ubuntu_install-prometheus-node-exporter.sh
+++ example-code/scripts/proxmox/research/post_install_prxmx_ubuntu_install-prometheus-node-exporter.sh
@@ -10,7 +10,7 @@ echo "Create system account and group node_exporter..."
 sudo adduser --system --group node_exporter
 
 echo "Copying extracted node_exporter to /usr/local/bin/..."
-cp -v ./node_exporter-1.4.0.linux-amd64/node_exporter /usr/local/bin/node_exporter
+sudo cp -v ./node_exporter-1.4.0.linux-amd64/node_exporter /usr/local/bin/node_exporter
 
 echo "Changing ownership of node_exporter binary..."
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
