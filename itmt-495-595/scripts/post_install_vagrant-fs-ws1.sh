69e42b1380bf89ecbea91b3a6306412eaebf5f34
Jeremy Hajek <hajek@iit.edu>
2023-01-06 10:23:03 -0600

    update removed itmd-521 assignments

--- itmt-495-595/scripts/post_install_vagrant-fs-ws1.sh
+++ itmt-495-595/scripts/post_install_vagrant-fs-ws1.sh
@@ -48,8 +48,8 @@ sudo hostnamectl set-hostname ws1
 
 # https://stackoverflow.com/questions/10175812/how-to-create-a-self-signed-certificate-with-openssl
 # https://ethitter.com/2016/05/generating-a-csr-with-san-at-the-command-line/
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048  -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=US/ST=IL/L=Chicago/O=IIT/OU=SAT/CN=class.edu"
-sudo openssl dhparam -out /etc/nginx/dhparam.pem 2048
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:4096  -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=US/ST=IL/L=Chicago/O=IIT/OU=SAT/CN=class.edu"
+sudo openssl dhparam -out /etc/nginx/dhparam.pem 4096
 # https://www.digitalocean.com/community/tutorials/how-to-create-a-self-signed-ssl-certificate-for-nginx-in-ubuntu-18-04
 sudo cp ./hajek/itmt-595/fullstack-app-code/nginx-ws/self-signed.conf /etc/nginx/snippets
 
