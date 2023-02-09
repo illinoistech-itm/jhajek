3cf71c543094e779500c13651ea2045fac42cb73
jeremy hajek <hajek@iit.edu>
2023-02-09 09:28:42 -0600

    Co-authored-by: Vinutha Bengaluru Prabhudev <vinuuth@users.noreply.github.com> Co-authored-by: Ritika77 <Ritika77@users.noreply.github.com> Co-authored-by: abhatt18 <abhatt18@users.noreply.github.com> Co-authored-by: Shiv Shankar N <shivshankar2996@users.noreply.github.com> Co-authored-by: Mayur Waghela <mayurwaghela5@users.noreply.github.com> Co-authored-by: Aditya Lodha <lodhaaditya12@users.noreply.github.com>

---
+++ itmd-521/example.scala
@@ -0,0 +1,33 @@
+package.main.scala.chapter3
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+
+object Example3_7 {
+    def main(args: Array[String]) {
+
+        val spark = SparkSession
+          .builder
+          .appName("Example3_7")
+          .getOrCreate()
+          
+        if (args.length <= 0){
+            println("usage Example_7 <file path to blogs.json>")
+            System.exit(1)
+        } // end of if
+
+        val jsonFile = args(0)
+        val schema = StructType(Array(StructField("Id",IntegerType, false), 
+            StructField("First", StringType. false),
+            StructField("Last",StringType,false),
+            StructField("Published",StringType,false),
+            StructType("Hits",IntegerType,false),
+            StructField("Campaigns", ArrayType(StringType), false)))
+
+        val blogsDF = spark.read.schema(schema).json(jsonFile)
+        blogsDF.show(false)
+        println(blogsDF.printSchema)
+        println(blogsDF.schema)
+        
+    } // end of def main
+} // end of object
+
