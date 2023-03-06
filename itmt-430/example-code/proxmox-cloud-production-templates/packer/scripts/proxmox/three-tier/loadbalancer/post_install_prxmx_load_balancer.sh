f4013ed0715dfd498bdaba402a7a4b412ba1bd2d
jeremy hajek <hajek@iit.edu>
2023-03-05 22:47:38 -0600

    update adding nginx config

--- itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/loadbalancer/post_install_prxmx_load_balancer.sh
+++ itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/loadbalancer/post_install_prxmx_load_balancer.sh
@@ -9,4 +9,3 @@ sudo apt-get install -y nginx
 # https://ethitter.com/2016/05/generating-a-csr-with-san-at-the-command-line/
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=US/ST=IL/L=Chicago/O=IIT/OU=rice/CN=iit.edu"
 sudo openssl dhparam -out /etc/nginx/dhparam.pem 2048
-
