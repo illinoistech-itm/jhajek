27cffffaf00d1da16460cb8ad79f40222b00ef6f
jeremy hajek <hajek@iit.edu>
2023-03-07 11:17:47 -0600

    update adding npm install

--- itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/frontend/application-start.sh
+++ itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/frontend/application-start.sh
@@ -2,5 +2,14 @@
 
 cd /home/vagrant/team-00/code/express-static-app/
 
+# run the npm install command to retrieve required express dependencies
+# this will retrieve all the NPM packages listed in the package.json file
+# and create the node_modules folder -- you don't want to be pushing 
+# node_modules around in version control
+npm install
+
+# pm2.io is an applcation service manager for Javascript applications
 sudo -u vagrant pm2 start server.js
+sudo -u vagrant pm2 save
+
 
