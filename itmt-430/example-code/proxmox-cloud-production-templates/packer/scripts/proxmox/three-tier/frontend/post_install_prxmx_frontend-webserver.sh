9d22cee0169eb718f92ef1ab4b3acafdc7cfee90
jeremy hajek <hajek@iit.edu>
2023-03-08 13:04:51 -0600

    fix pm2 start up

--- itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_frontend-webserver.sh
+++ itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_frontend-webserver.sh
@@ -15,7 +15,8 @@ sudo npm install -g npm@9.6.0
 # Upgrade to latest NPM
 sudo npm install -g express ejs pm2
 # pm2.io is an applcation service manager for Javascript applications
+# Change directory to the location of your JS code
 cd /home/vagrant/team-00/code/express-static-app/
-sudo pm2 start server.js
-sudo pm2 save
-
+# sudo pm2 start server.js
+# This creates your javascript application service file
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u vagrant --hp /home/vagrant
