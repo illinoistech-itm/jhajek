95bcb7e4b2b978a80a54942263bc7165369a78ea
jeremy hajek <hajek@iit.edu>
2023-03-12 23:18:26 -0500

    update adding echo

--- itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/frontend/application-start.sh
+++ itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/frontend/application-start.sh
@@ -6,4 +6,5 @@ cd /home/vagrant/team-00/code/express-static-app/
 # this will retrieve all the NPM packages listed in the package.json file
 # and create the node_modules folder -- you don't want to be pushing 
 # node_modules around in version control
+echo "Installing the javascript packages for ejs..."
 sudo npm install
