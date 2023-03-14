4b282804ac293299c07e27c0d85245444f948de7
jeremy hajek <hajek@iit.edu>
2023-03-13 21:37:38 -0500

    update mariadb install version specifics

--- itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/backend/post_install_prxmx_backend-database.sh
+++ itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/backend/post_install_prxmx_backend-database.sh
@@ -3,7 +3,7 @@
 # Install and prepare backend database
 
 sudo apt update
-sudo apt-get install -y mariadb-server
+sudo apt-get install -y mariadb-server-10.6
 
 ## During the Terraform apply phase -- we will make some run time adjustments
 # to configure the database to listen on the meta-network interface only
