affe57e97f89fb9d98627d11f387f751f3dcfb42
Jeremy Hajek <hajek@iit.edu>
2023-03-29 14:04:23 -0500

    update week 11

---
+++ itmd-321/week-11/week-11-lab-and-questions.sql
@@ -0,0 +1,23 @@
+-- Use the Sakila database - write a single file named week-11-lab.sql
+
+-- Use `--` for single line comments and `/* */` for multiline comments.  For questions requiring a text answer - use a comment command. For SQL commands - place the output of the SQL command in a comment below the question
+
+-- What command will show the schema of the staff table?
+
+-- What command will show the schema of the address table?
+
+-- Does the address table have any foreign keys?  If so list them
+
+-- Does the sales_by_store table have any foreign keys? If so list them
+
+-- Write an SQL query to join first_name, last_name, and address of all staff members
+
+-- Write an SQL query to select all customers, store_id, last_name, and first_name
+
+-- Write an SQL query to select all customers with active accounts
+
+-- Write an SQL query to find all customer first_name and last_name where the last_name starts with "A" and the city they live in
+
+-- Write an SQL query to find the first_name and last_name of the manager of each of the rental stores
+
+-- Write an SQL query to find all rentals bu users with the firstname and lastname "RENE" AND "MCALISTER" and "ROSS" AND "GREY"
