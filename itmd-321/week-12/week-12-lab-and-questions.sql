d96e920622ef692985f46e9d20510a524a88fca2
Jeremy Hajek <hajek@iit.edu>
2023-04-05 11:37:07 -0500

    update week-12 lab and questions

---
+++ itmd-321/week-12/week-12-lab-and-questions.sql
@@ -0,0 +1,20 @@
+-- Write an SQL query to show the customer (first name and last) and count the number of rentals - group by customer_id
+
+-- Write an SQL query to list the name of the actor and counts of all the G and PG films they have been in
+
+-- Write an SQL query to find the number of films in: English, Italian, Japanese, Mandarin, French, German   group by lanuage - print out count and lanuguage
+
+-- Write an SQL query to find the customer name (first and last) that has the highest payment per country
+
+-- Write an SQL query to find the AVG number of movies that actors are in (from the actor and film tables)
+
+-- Write an SQL query to find the Highest number of movies that an actors is in - show actor name and the max count.
+
+-- Write an SQL query to show Customer information (first and lastname, address and having rented more than 20 movies)
+
+-- Write an SQL query to complete Exercise 8-1 from the textbook
+
+-- Write an SQL query to complete Exercise 8-2 from the textbook
+
+-- Write an SQL query to complete Exercise 8-3 from the textbook
+
