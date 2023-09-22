640725fb2e26cde83da1b739ead62dd8e12233f6
Jeremy Hajek <hajek@iit.edu>
2023-09-22 11:31:53 -0500

    update the sample code to query only running InstanceIds

--- itmo-444-544/week-05/create-env.sh
+++ itmo-444-544/week-05/create-env.sh
@@ -15,7 +15,11 @@ VPCID=$(aws ec2 describe-vpcs --query "Vpcs[].VpcId")
 SUBNET=$(aws ec2 describe-subnets --output=json | jq -r '.Subnets[1,2].SubnetId')
 
 aws ec2 run-instances --image-id $1 --instance-type $2 --key-name $3 --security-group-ids $4 --count ${5} --user-data file://$6
-INSTANCEIDS=$(aws ec2 describe-instances --output=json | jq -r '.Reservations[].Instances[].InstanceId')
+# Using jq from the command line
+# INSTANCEIDS=$(aws ec2 describe-instances --output=json | jq -r '.Reservations[].Instances[].InstanceId')
+
+# Using aws --query functions to query for the InstanceIds of only RUNNING instances
+INSTANCEIDS=$(aws ec2 describe-instances --query 'Reservations[*].Instances[?State.Name==`running`].InstanceId')
 
 aws elbv2 create-load-balancer --name $8 --subnets $SUBNET
 ELBARN=$(aws elbv2 describe-load-balancers --output=json | jq -r '.LoadBalancers[].LoadBalancerArn')
