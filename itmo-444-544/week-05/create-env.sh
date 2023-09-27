1080ab9ed02892da3b594584547ffa2ebc2b365f
Jeremy Hajek <hajek@iit.edu>
2023-09-27 15:42:21 -0500

    Co-authored-by: YashReddy <yashreddy21@users.noreply.github.com> Co-authored-by: Daniela Munoz <DMunoz-832@users.noreply.github.com> Co-authored-by: Cardiinale <Cardiinale@users.noreply.github.com> Co-authored-by: radfre <radfre@users.noreply.github.com> Co-authored-by: pranitp2222 <pranitp2222@users.noreply.github.com> Co-authored-by: Andrei Neacsu <prycaustic@users.noreply.github.com> Co-authored-by: Naga Prasath Saravanan <nagap1126@users.noreply.github.com>

--- itmo-444-544/week-05/create-env.sh
+++ itmo-444-544/week-05/create-env.sh
@@ -6,6 +6,7 @@
 # Pranit Patil
 # Ssanidhya Barraptay
 # Daniela Munoz 
+# Alex Schatz
 # Michael Martinez
 # Naga Prasath
 # Amith Satyanarayan
@@ -14,7 +15,7 @@
 VPCID=$(aws ec2 describe-vpcs --query "Vpcs[].VpcId")
 SUBNET=$(aws ec2 describe-subnets --output=json | jq -r '.Subnets[1,2].SubnetId')
 
-aws ec2 run-instances --image-id $1 --instance-type $2 --key-name $3 --security-group-ids $4 --count ${5} --user-data file://$6
+aws ec2 run-instances --image-id $1 --instance-type $2 --key-name $3 --security-group-ids $4 --count ${5} --user-data file://$6 --availability-zone $7
 # Using jq from the command line
 # INSTANCEIDS=$(aws ec2 describe-instances --output=json | jq -r '.Reservations[].Instances[].InstanceId')
 
@@ -22,7 +23,7 @@ aws ec2 run-instances --image-id $1 --instance-type $2 --key-name $3 --security-
 # https://docs.aws.amazon.com/cli/latest/userguide/cli-usage-filter.html 
 INSTANCEIDS=$(aws ec2 describe-instances --query 'Reservations[*].Instances[?State.Name==`running`].InstanceId')
 
-aws elbv2 create-load-balancer --name $8 --subnets $SUBNET
+aws elbv2 create-load-balancer --name $8 --subnets $SUBNET --type application --security-groups $4
 ELBARN=$(aws elbv2 describe-load-balancers --output=json | jq -r '.LoadBalancers[].LoadBalancerArn')
 
 aws elbv2 wait load-balancer-available --load-balancer-arns $ELBARN
@@ -31,17 +32,25 @@ aws elbv2 wait load-balancer-available --load-balancer-arns $ELBARN
 TARGETARN=$(aws elbv2 create-target-group --name $9 --protocol HTTP --port 80 --target-type instance --vpc-id $VPCID --output=json | jq -r '.TargetGroups[].TargetGroupArn')
 
 # https://docs.aws.amazon.com/cli/latest/reference/elbv2/register-targets.html
-aws elbv2 register-targets --target-group-arn $TARGETARN --targets $INSTANCEIDS
+# For loop that goes takes every value in INSTANCEIDS and puts it in IIDS 
+for IIDS in $INSTANCEIDS;
+do aws elbv2 register-targets --target-group-arn $TARGETARN --targets Id=$IIDS;
+done
+
+#Attach target group to ELB listener
+#The listener is how AWS knows that a target group is accesible, without it the next wait target-in-service will not see anything and will be stuck
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/create-listener.html
+aws elbv2 create-listener --load-balancer-arn $ELBARN --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$TARGETARN
 
 # Create waiter for registering targets
 # https://docs.aws.amazon.com/cli/latest/reference/elbv2/wait/target-in-service.html
 aws elbv2 wait target-in-service --target-group-arn $TARGETARN
 
-#Attach target group to ELB listener
-# https://docs.aws.amazon.com/cli/latest/reference/elbv2/create-listener.html
-aws elbv2 create-listener --load-balancer-arn $ELBARN --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$TARGETARN
-
 # Describe ELB - find the URL of the load-balancer
+
+URL=$(aws elbv2 describe-load-balancers --output=json | jq -r  '.LoadBalancers[].DNSName')
+
+echo $URL
 
 # NOTES
 # https://docs.aws.amazon.com/cli/latest/
