9dd61801aa8b24aa181a56ea62670c6b15bbca35
Jeremy Hajek <hajek@iit.edu>
2023-10-23 15:46:02 -0500

    Co-authored-by: Andrei Neacsu <prycaustic@users.noreply.github.com> Co-authored-by: Naga Prasath Saravanan <nagap1126@users.noreply.github.com> Co-authored-by: CameronAGluth <CameronAGluth@users.noreply.github.com> Co-authored-by: Daniela Munoz <DMunoz-832@users.noreply.github.com> Co-authored-by: radfre <radfre@users.noreply.github.com> Co-authored-by: pranitp2222 <pranitp2222@users.noreply.github.com>

--- itmo-444-544/week-09/install-env.sh
+++ itmo-444-544/week-09/install-env.sh
@@ -1,3 +1,60 @@
 #!/bin/bash
 
+##############################################################################
+# Installing Git client from apt
+##############################################################################
+sudo apt update
+sudo apt install -y git build-essential
+
+##############################################################################
+# Installing Node JS via DEB packages from 
+# https://github.com/nodesource/distributions#nodejs
+##############################################################################
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update
+sudo apt-get install nodejs -y
+##############################################################################
+# Tricks needed to make sure we have the correct permissions
+# sudo -u ubuntu npm install @aws-sdk/client-s3@3.202.0 express multer multer-s3
+# sudo npm install pm2 -g
+##############################################################################
+# Install multer s3
+# -g = global
+##############################################################################
+npm install --save multer-s3 -g
+
+##############################################################################
+#Install the AWS v3 JavaScript SDK
+##############################################################################
+npm init -y  # command to create a default package.json file
+npm i @aws-sdk/client-s3
+##############################################################################
+# Code to install pm2 nodejs service start tool
+##############################################################################
+
+npm install pm2 -g
+
+##############################################################################
+# Code to install express via npm
+##############################################################################
+npm install express
+
+#############################################################################
+
+##############################################################################
+# Clone GitHub Repo with project week-09 source code
+##############################################################################
 sudo -u ubuntu git clone git@github.com:illinoistech-itm/hajek.git
+
+# Start the nodejs app where it is located via PM2
+# https://pm2.keymetrics.io/docs/usage/quick-start
+cd /home/ubuntu/YOUR-HAWKID/itmo-544/mp1/
+
+sudo pm2 start app.js
