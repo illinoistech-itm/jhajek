ac7d5cf467cc4a9a968eb454b048ad29110cdc30
Jeremy Hajek <hajek@iit.edu>
2023-11-01 16:08:57 -0500

    added default copy

--- itmo-444-544/week-09/install-env.sh
+++ itmo-444-544/week-09/install-env.sh
@@ -18,6 +18,7 @@ sudo systemctl start nginx
 # https://stackoverflow.com/questions/6537490/insert-a-line-at-specific-line-number-with-sed-or-awk
 ##############################################################################
 sudo sed -i '54i location /upload {\n proxy_pass http://127.0.0.1:3000/upload/; \n }' /etc/nginx/sites-available/default
+
 # Reload the nginx config
 sudo systemctl daemon-reload
 sudo systemctl restart nginx
@@ -72,5 +73,10 @@ sudo -u ubuntu git clone git@github.com:illinoistech-itm/hajek.git
 # Start the nodejs app where it is located via PM2
 # https://pm2.keymetrics.io/docs/usage/quick-start
 cd /home/ubuntu/YOUR-HAWKID/itmo-544/mp1/
+
+# sudo cp /home/ubuntu/YOUR_HAWK_ID/itmo-544/week-09/default /etc/nginx/sites-available/default
+# sudo systemctl daemon-reload
+# sudo systemctl restart nginx
+
 
 sudo pm2 start app.js
