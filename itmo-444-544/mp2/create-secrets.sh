ae858ee7e8fd63b1a4f02762ee11bed2aaaaf542
Jeremy Hajek <hajek@iit.edu>
2023-11-06 15:49:33 -0600

    Co-authored-by: Cardiinale <Cardiinale@users.noreply.github.com> Co-authored-by: radfre <radfre@users.noreply.github.com> Co-authored-by: Daniela Munoz <DMunoz-832@users.noreply.github.com> Co-authored-by: Andrei Neacsu <prycaustic@users.noreply.github.com> Co-authored-by: Naga Prasath Saravanan <nagap1126@users.noreply.github.com> Co-authored-by: Nicholas Simpkins <nicksimpkins@users.noreply.github.com> Co-authored-by: asatyanarayan1 <asatyanarayan1@users.noreply.github.com>

--- itmo-444-544/mp2/create-secrets.sh
+++ itmo-444-544/mp2/create-secrets.sh
@@ -6,5 +6,5 @@ echo "creating secret..."
 # Add maria.json to your .gitignore file on your host system, push this to GitHub
 # Modify your maria-template.json not on the local system but on your Vagrant Box after you have
 # issued a git pull - rename the maria-template.json to maria.json and add a username and password
-aws secretsmanager create-secret --name ${20} --secret-string file://maria.json
+aws secretsmanager create-secret --name ${21} --secret-string file://maria.json
 
