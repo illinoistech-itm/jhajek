dfacaead90141aa32d7e6eae8548f0143ce3b829
Jeremy Hajek <hajek@iit.edu>
2023-11-26 00:29:02 -0600

    rem,oved var

--- itmo-444-544/mp2/terraform/main.tf
+++ itmo-444-544/mp2/terraform/main.tf
@@ -49,7 +49,7 @@ resource "aws_lb" "alb" {
   name               = var.elb-name
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [var.var.vpc_security_group_ids]
+  security_groups    = [var.vpc_security_group_ids]
   subnets            = [for subnet in data.aws_subnet.example : subnet.id]
 
   enable_deletion_protection = true
