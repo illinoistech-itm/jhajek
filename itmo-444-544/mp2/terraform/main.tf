5818dcb8657a42297c4d1ec61508ff752ba81ee6
Jeremy Hajek <hajek@iit.edu>
2023-11-26 16:03:22 -0600

    removed extra s

--- itmo-444-544/mp2/terraform/main.tf
+++ itmo-444-544/mp2/terraform/main.tf
@@ -35,14 +35,14 @@ data "aws_subnets" "subnets" {
 
 data "aws_subnet" "az0-subnet0" {
   filter {
-    name = "availablity_zones"
+    name = "availablity_zone"
     values = ["us-east-2a"]
   }
 }
 
 data "aws_subnet" "az1-subnet1" {
   filter {
-    name = "availablity_zones"
+    name = "availablity_zone"
     values = ["us-east-2b"]
   }
 }
