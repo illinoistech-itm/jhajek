4b4887df80e009ef0611efdaae1bb75b328eefa0
Jeremy Hajek <hajek@iit.edu>
2023-11-26 22:42:52 -0600

    add id back

--- itmo-444-544/mp2/terraform/main.tf
+++ itmo-444-544/mp2/terraform/main.tf
@@ -98,7 +98,7 @@ resource "aws_lb_target_group" "alb-lb-tg" {
   target_type = "alb"
   port        = 80
   protocol    = "TCP"
-  vpc_id      = data.aws_vpcs.main.
+  vpc_id      = data.aws_vpcs.main.id
 }
 
 ##############################################################################
