3b7ac2b2f7f9c92abc3148e0500cd65813f1e487
Jeremy Hajek <hajek@iit.edu>
2023-11-28 12:50:08 -0600

    update 444 mp2

--- itmo-444-544/mp2/444/install-env.sh
+++ itmo-444-544/mp2/444/install-env.sh
@@ -2,7 +2,11 @@
 
 # Install dependecies here:
 
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && sudo apt-get install -y nodejs
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt install -y nginx 
 
 # Run NPM to install the NPM Node packages needed for the code
 # You will start this NodeJS script by executing the command: node app.js
@@ -12,7 +16,7 @@ curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && sudo apt-ge
 # <https://pm2.keymetrics.io/docs/usage/quick-start/>. This will require
 # the install of PM2 via npm as well.
 cd /home/ubuntu
-sudo -u ubuntu npm install express aws-sdk multer multer-s3
+sudo -u ubuntu npm install @aws-sdk/client-s3 @aws-sdk/client-sns @aws-sdk/client-rds @aws-sdk/client-secrets-manager express multer multer-s3 mysql2
 sudo npm install pm2 -g
 
 # Command to clone your private repo via SSH usign the Private key
@@ -21,9 +25,16 @@ sudo npm install pm2 -g
 ####################################################################
 sudo -u ubuntu git clone git@github.com:illinoistech-itm/jhajek.git
 
+#######################################################################################
+# Copy over your default Nginx configuration file to provide routes for /db /gallery and /upload
+sudo cp /home/ubuntu/jhajek/itmo-444-544/mp2/444/default /etc/nginx/sites-available/default
+# Reload the nginx config with your changes
+sudo systemctl daemon-reload
+sudo systemctl restart nginx
+
 # Start the nodejs app where it is located via PM2
 # https://pm2.keymetrics.io/docs/usage/quick-start
-cd /home/ubuntu/jhajek/itmo-444-544/mp2/444
+cd /home/ubuntu/jhajek/itmo-444-544/mp2/444/
 
 sudo pm2 start app.js
 
