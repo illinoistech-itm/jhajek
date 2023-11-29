da74a366804d61427b7eff987448715d9495aacf
Jeremy Hajek <hajek@iit.edu>
2023-11-28 22:19:58 -0600

    update mp2

---
+++ itmo-444-544/mp2/444/create-dynamodb-table.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/dynamodb/create-table.html
+
+# Table name now has to be uniqu
+# Attribute definitions are only needed for the Hash-Key and Range Key - this is a NoSQL so there is no schema
+
+# -- sql schema
+#ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+#RecordNumber VARCHAR(64), -- This is the UUID
+#CustomerName VARCHAR(64),
+#Email VARCHAR(64),
+#Phone VARCHAR(64),
+#Stat INT(1) DEFAULT 0, -- Job status, not done is 0, done is 1
+#RAWS3URL VARCHAR(200), -- set the returned S3URL here
+#FINSIHEDS3URL VARCHAR(200)
+
+IDENTITY=$(aws sts get-caller-identity --output json --query='Arn')
+echo "Identity: $IDENTITY"
+
+aws dynamodb create-table \
+    --table-name ${23} \
+    --attribute-definitions AttributeName=Email,AttributeType=S AttributeName=RecordNumber,AttributeType=S \
+    --key-schema AttributeName=Email,KeyType=HASH AttributeName=RecordNumber,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --tags Key=Owner,Value=$IDENTITY
+
+
+# Put Item for DynomoDB -- like an SQL INSERT command
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/dynamodb/put-item.html
+# The test record we are inserting here is defined in an external file: item.json
+
+aws dynamodb put-item \
+    --table-name ${23} \
+    --item file://item.json \
+    --return-consumed-capacity TOTAL \
+    --return-item-collection-metrics SIZE
+
+# Query your DynamoDB table for records
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/dynamodb/query.html
+aws dynamodb query \
+    --table-name ${23} \
+    --projection-expression "RecordNumber" \
+    --key-condition-expression "Email = :v1" \
+    --expression-attribute-values file://expression-attributes.json \
+    --return-consumed-capacity TOTAL
+
--- itmo-444-544/mp2/444/create.sql
+++ itmo-444-544/mp2/444/create.sql
@@ -11,7 +11,7 @@ Email VARCHAR(64),
 Phone VARCHAR(64),
 Stat INT(1) DEFAULT 0, -- Job status, not done is 0, done is 1
 RAWS3URL VARCHAR(200), -- set the returned S3URL here
-FINSIHEDS3URL VARCHAR(200)
+FINISHEDS3URL VARCHAR(200)
 );
 
 INSERT INTO entries(RecordNumber,CustomerName,Email,Phone,Stat,RAWS3URL) VALUES('00000',"NAME","email@iit.edu","000-000-0000",0,"http://");
