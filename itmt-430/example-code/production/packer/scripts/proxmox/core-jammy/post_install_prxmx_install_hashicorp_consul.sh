0892a45fc2494416a77e69aafb167d5860344842
Jeremy <hajek@iit.edu>
2024-02-25 21:00:22 -0600

    update missing URL

--- itmt-430/example-code/production/packer/scripts/proxmox/core-jammy/post_install_prxmx_install_hashicorp_consul.sh
+++ itmt-430/example-code/production/packer/scripts/proxmox/core-jammy/post_install_prxmx_install_hashicorp_consul.sh
@@ -4,7 +4,7 @@
 
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 #echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] http://10.0.0.40 $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] http://10.0.0.40/hashicorp $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt-get update && sudo apt-get install -y consul
 
 sudo systemctl stop consul.service
