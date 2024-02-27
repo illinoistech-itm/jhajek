0e8c1cd93f9041c04ed22a9607b3442b316d5311
Jeremy <hajek@iit.edu>
2024-02-26 20:46:21 -0600

    update to local mirror for gpg

--- itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/core-jammy/post_install_prxmx_install_hashicorp_consul.sh
+++ itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/core-jammy/post_install_prxmx_install_hashicorp_consul.sh
@@ -2,7 +2,7 @@
 
 # script to install hashicorp consul for Proxmox servers
 
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+wget -O- http://10.0.0.40/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 #echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] http://10.0.0.40/hashicorp $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt-get update && sudo apt-get install -y consul
