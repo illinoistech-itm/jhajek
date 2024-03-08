a52363b9d406c9bf85a1cdf800ceff8758f40c44
Jeremy <hajek@iit.edu>
2024-03-08 15:04:07 -0600

    update packer 1.1.7

--- itmo-444-544/mp1/create-env-database-and-secrets-example.sh
+++ itmo-444-544/mp1/create-env-database-and-secrets-example.sh
@@ -2,8 +2,8 @@
 
 aws secretsmanager create-secret --name $AWS_SECRET_NAME --secret-string file://maria.json
 SECRET_ID=$(aws secretsmanager list-secrets --filters Key=name,Values=$AWS_SECRET_NAME --query 'SecretList[*].ARN')
-USERVALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --output=json | jq '.SecretString' | tr -s , ' ' | tr -s ['"'] ' ' | awk {'print $6'} |  tr -d '\\')
-PASSVALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --output=json | jq '.SecretString' | tr -s } ' ' | tr -s ['"'] ' ' | awk {'print $12'} | tr -d '\\')
+USERVALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --output=json | jq '.SecretString' | sed 's/[\\]//g' | sed 's/^"//g' | sed 's/"$//g' | jq '.username' | sed 's/"//g')
+PASSVALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --output=json | jq '.SecretString' | sed 's/[\\]//g' | sed 's/^"//g' | sed 's/"$//g' | jq '.password' | sed 's/"//g')
 
 
 aws rds wait db-instance-available \
