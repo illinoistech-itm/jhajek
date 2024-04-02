e1c72fabbd6eac12b25004cc2ad81e1c561648a6
Jeremy <hajek@iit.edu>
2024-04-02 11:59:50 -0500

    add last part to rename dir

--- itmd-521/cluster-work/core-jammy/7_get_spark.sh
+++ itmd-521/cluster-work/core-jammy/7_get_spark.sh
@@ -6,6 +6,12 @@ wget https://dlcdn.apache.org/spark/spark-3.5.1/spark-3.5.1-bin-hadoop3.tgz
 mv ./spark-3.5.1-bin-hadoop3.tgz /home/controller/spark-3.5.1-bin-hadoop3.tgz
 sudo chown -R controller:controller /home/controller/spark-3.5.1-bin-hadoop3.tgz
 
+cd /home/controller
+
+tar -xvzf /home/controller/spark-3.5.1-bin-hadoop3.tgz
+mv /home/controller/spark-3.5.1-bin-hadoop3.tgz /home/controller/spark
+
+
 cat << EOF >> /home/controller/.bashrc
 export SPARK_HOME=/home/controller/spark
 export SPARK_WORKER_DIR=/datapool2/worker-dir
