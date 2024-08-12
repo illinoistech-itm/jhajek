809a91ca7f40d76d6f2a26917e740908faae8f0a
Jeremy <hajek@iit.edu>
2024-08-11 22:32:21 -0500

    updat einstall

--- itmo-444-544/mp2/444/install-env.sh
+++ itmo-444-544/mp2/444/install-env.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Install dependecies here:
-
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt install -y nginx 
+sudo systemctl enable --now nginx
+##############################################################################
+# Install Node JS
+# https://github.com/nodesource/distributions#installation-instructions-deb
+##############################################################################
+curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install -y nodejs
+node -v
+
+
 
 # Run NPM to install the NPM Node packages needed for the code
 # You will start this NodeJS script by executing the command: node app.js
