1b582be44a3ce8c1f4f64f5ecb6b03340425f266
Jeremy <hajek@iit.edu>
2024-09-03 19:45:54 -0500

    update code newer PDF version -- buy the PDF

--- itmd-521/labs/week-03/mnmcount.py
+++ itmd-521/labs/week-03/mnmcount.py
@@ -1,6 +1,7 @@
 import sys
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import count
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -12,12 +13,12 @@ if __name__ == "__main__":
 
     mnm_df = (spark.read.format("csv").option("header","true").option("inferSchema", "true").load(mnm_file))
 
-    count_mnm_df = (mnm_df.select("State", "Color","Count").groupBy("State","Color").sum("Count").orderBy("sum(Count)", ascending=False))
+    count_mnm_df = (mnm_df.select("State", "Color","Count").groupBy("State","Color").agg(count("Count").alias("Total")).orderBy("Total", ascending=False))
 
     count_mnm_df.show(n=60, truncated=False)
     print("Total Rows = %d" % (count_mnm_df.count()))
 
-    ca_count_mnm_df = (mnm_df.select("State","Color","Count").where(mnm_df.State == "CA").sum("Count").orderBy("sum(Count)", ascending=False))
+    ca_count_mnm_df = (mnm_df.select("State","Color","Count").where(mnm_df.State == "CA").agg(count("Count").alias("Total")).orderBy("Total", ascending=False))
 
     ca_count_mnm_df.show(n=10, truncate=False)
 
