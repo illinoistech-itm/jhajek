e01ad8b2a03c27be903a4a8a37b5b496eeb1d36a
Jeremy <hajek@iit.edu>
2024-09-12 10:20:08 -0500

    update fix

--- itmo-444-544/weekly-assignments/module-03/create-env.sh
+++ itmo-444-544/weekly-assignments/module-03/create-env.sh
@@ -2,4 +2,4 @@
 
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
 echo ${4}
-aws ec2 run-instances --image-id ${1} --instance-type ${2}  --key-name ${3} --security-group-ids ${4} --count ${5} --user-data file://${6} 
+aws ec2 run-instances --image-id ${1} --instance-type ${2}  --key-name ${3} --security-group-ids ${4} --count ${5} --user-data file://${6}
