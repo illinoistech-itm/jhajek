1b11994a6a2ca3a9cacbc2a6277268efcff06874
Jeremy <hajek@iit.edu>
2024-09-12 09:49:16 -0500

    remove tag spec

--- itmo-444-544/weekly-assignments/module-03/create-env.sh
+++ itmo-444-544/weekly-assignments/module-03/create-env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
-aws ec2 run-instances --image-id ${1} --instance-type ${2}  --key-name ${3} --security-group-ids ${4} --count ${5} --user-data file://${6} --tag-specifications ResourceType=Instances,Tags=[{Key=Name,Value=${10}}]
+aws ec2 run-instances --image-id ${1} --instance-type ${2}  --key-name ${3} --security-group-ids ${4} --count ${5} --user-data file://${6} 
