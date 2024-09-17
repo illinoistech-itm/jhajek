21fb9b52eb93a1e0453a4c3ab5feed7c9c455334
Jeremy <hajek@iit.edu>
2024-09-17 09:45:48 -0500

    Co-authored-by: KaushalNai <KaushalNai@users.noreply.github.com> Co-authored-by: Monil Shah <Monil2310@users.noreply.github.com> Co-authored-by: Rahul Nagaraju <rahulnagaraju724@users.noreply.github.com> Co-authored-by: BrahmantyaWardhana <BrahmantyaWardhana@users.noreply.github.com> Co-authored-by: Kevin-Alexis-Aguilar <Kevin-Alexis-Aguilar@users.noreply.github.com> Co-authored-by: hrajcharan <hrajcharan@users.noreply.github.com> Co-authored-by: Siddartha1002 <Siddartha1002@users.noreply.github.com> Co-authored-by: Shantanu Meshram <shantanum28@users.noreply.github.com>

---
+++ itmo-444-544/weekly-assignments/module-04/destroy-env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Dynamically detect your infrastrcuture and destroy it/terminate it
+# SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${12}")
+# First Query to get the ELB name using the --query and --filters
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-listeners.html
+ELBARN=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerARN' --region=${})
+
+#Delete loadbalancer
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/delete-load-balancer.html
+aws elbv2 delete-load-balancers --load-balancer-arn $ELBARN
+
+# First Describe EC2 instances
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html
+aws ec2 describe-instances \
+    --output=text \
+    --query='Reservations[*].Instances[*].InstanceId'
+
+# Now Terminate all EC2 instances
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/terminate-instances.html
+
+
