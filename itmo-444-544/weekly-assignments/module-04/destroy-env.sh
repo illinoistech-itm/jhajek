dd9149240a04bf6c352f52cffd3eb6cc29d6261f
Jeremy <hajek@iit.edu>
2024-09-19 08:55:57 -0500

    Co-authored-by: Monil Shah <Monil2310@users.noreply.github.com> Co-authored-by: Siddartha1002 <Siddartha1002@users.noreply.github.com> Co-authored-by: BrahmantyaWardhana <BrahmantyaWardhana@users.noreply.github.com> Co-authored-by: vamsi-k-bsns <vamsi-k-bsns@users.noreply.github.com> Co-authored-by: KaushalNai <KaushalNai@users.noreply.github.com> Co-authored-by: Kat Prush <kdprush@users.noreply.github.com> Co-authored-by: Rahul Nagaraju <rahulnagaraju724@users.noreply.github.com> Co-authored-by: Kevin-Alexis-Aguilar <Kevin-Alexis-Aguilar@users.noreply.github.com> Co-authored-by: simranmhaske <simranmhaske@users.noreply.github.com> Co-authored-by: Shantanu Meshram <shantanum28@users.noreply.github.com> Co-authored-by: divyeshh <divyeshh@users.noreply.github.com> Co-authored-by: nkchai <nkchai@users.noreply.github.com>

--- itmo-444-544/weekly-assignments/module-04/destroy-env.sh
+++ itmo-444-544/weekly-assignments/module-04/destroy-env.sh
@@ -4,7 +4,7 @@
 # SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${12}")
 # First Query to get the ELB name using the --query and --filters
 # https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-listeners.html
-ELBARN=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerARN' --region=${})
+ELBARN=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerARN')
 
 #Delete loadbalancer
 # https://docs.aws.amazon.com/cli/latest/reference/elbv2/delete-load-balancer.html
@@ -12,11 +12,11 @@ aws elbv2 delete-load-balancers --load-balancer-arn $ELBARN
 
 # First Describe EC2 instances
 # https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html
-aws ec2 describe-instances \
+EC2IDS=$(aws ec2 describe-instances \
     --output=text \
-    --query='Reservations[*].Instances[*].InstanceId'
+    --query='Reservations[*].Instances[*].InstanceId')
 
 # Now Terminate all EC2 instances
 # https://docs.aws.amazon.com/cli/latest/reference/ec2/terminate-instances.html
-
+ aws ec2 terminate-instances --instance-ids $EC2IDS
 
