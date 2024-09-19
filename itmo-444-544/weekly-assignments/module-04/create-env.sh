3c4988f3a59059b54b81edb3977c09eb61766586
Jeremy <hajek@iit.edu>
2024-09-19 09:31:06 -0500

    updated to add waiters

--- itmo-444-544/weekly-assignments/module-04/create-env.sh
+++ itmo-444-544/weekly-assignments/module-04/create-env.sh
@@ -24,12 +24,34 @@ echo $SUBNET2A
 echo $SUBNET2B
 echo $SUBNET2C
 
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/create-load-balancer.html
 aws elbv2 create-load-balancer \
     --name ${8} \
     --subnets $SUBNET2A $SUBNET2B $SUBNET2C \
     --tags Key='name',Value=${13} 
 
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-listeners.html
+ELBARN=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerArn')
+echo "*****************************************************************"
+echo "Printing ELBARN: $ELBARN"
+echo "*****************************************************************"
 
+# add elv2 wait running reference
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/wait/
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/load-balancer-available.html
+echo "Waiting for ELB to become available..."
+aws elbv2 wait load-balancer-available --load-balancer-arns $ELBARN
+echo "ELB is available..."
 
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
 aws ec2 run-instances --image-id ${1} --instance-type ${2}  --key-name ${3} --security-group-ids ${4} --count ${5} --user-data file://${6}
+
+echo "Retrieving Instance ID"
+EC2IDS=$(aws ec2 describe-instances \
+    --output=text \
+    --query='Reservations[*].Instances[*].InstanceId' --filter Name=instance-state-name,Values=pending,running)
+
+echo "Waiting for instances..."
+#https://docs.aws.amazon.com/cli/latest/reference/ec2/wait/instance-running.html
+aws ec2 wait instance-running --instance-ids $EC2IDS
+echo "Instances are up!"
--- itmo-444-544/weekly-assignments/module-04/destroy-env.sh
+++ itmo-444-544/weekly-assignments/module-04/destroy-env.sh
@@ -5,8 +5,9 @@
 # First Query to get the ELB name using the --query and --filters
 # https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-listeners.html
 ELBARN=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerArn')
+echo "*****************************************************************"
 echo "Printing ELBARN: $ELBARN"
-
+echo "*****************************************************************"
 #Delete loadbalancer
 # https://docs.aws.amazon.com/cli/latest/reference/elbv2/delete-load-balancer.html
 aws elbv2 delete-load-balancer --load-balancer-arn $ELBARN
@@ -15,9 +16,9 @@ aws elbv2 delete-load-balancer --load-balancer-arn $ELBARN
 # https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html
 EC2IDS=$(aws ec2 describe-instances \
     --output=text \
-    --query='Reservations[*].Instances[*].InstanceId')
+    --query='Reservations[*].Instances[*].InstanceId' --filter Name=instance-state-name,Values=pending,running  )
 
 # Now Terminate all EC2 instances
 # https://docs.aws.amazon.com/cli/latest/reference/ec2/terminate-instances.html
  aws ec2 terminate-instances --instance-ids $EC2IDS
-
+echo "Instances are terminated!"
