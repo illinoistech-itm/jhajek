21dababe5f4dd7495750cf826c6de8214ab1270f
Jeremy <hajek@iit.edu>
2024-09-20 14:54:15 -0500

    uodate disk number

--- itmt-430/example-code/production/terraform/proxmox-jammy-ubuntu-vault-infra/main.tf
+++ itmt-430/example-code/production/terraform/proxmox-jammy-ubuntu-vault-infra/main.tf
@@ -8,7 +8,8 @@ resource "random_id" "id" {
 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/shuffle#example-usage
 resource "random_shuffle" "datadisk" {
-  input        = ["datadisk1","datadisk2","datadisk3","datadisk4"]
+  #input        = ["datadisk1","datadisk2","datadisk3","datadisk4"]
+  input        = ["datadisk1","datadisk2"]
   result_count = 1
 }
 
