186a63960063040138c1f3955e29acf9f192bec1
Jeremy <hajek@iit.edu>
2024-09-20 22:52:47 -0500

    update template

--- itmt-430/example-code/production/terraform/proxmox-jammy-ubuntu-infra/provider.tf
+++ itmt-430/example-code/production/terraform/proxmox-jammy-ubuntu-infra/provider.tf
@@ -21,7 +21,7 @@ provider "vault" {}
 
 provider "proxmox" {
   pm_tls_insecure     = true
-  pm_api_url          = data.vault_generic_secret.pm_api_url.data["SYSTEM41"]
+  pm_api_url          = data.vault_generic_secret.pm_api_url.data["SYSTEM35"]
   pm_api_token_id     = data.vault_generic_secret.pm_api_token_id.data["USERNAME"]
   pm_api_token_secret = data.vault_generic_secret.pm_api_token_secret.data["TOKEN"]
   pm_log_enable       = var.pm_log_enable
