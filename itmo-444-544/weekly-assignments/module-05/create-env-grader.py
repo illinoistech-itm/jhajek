0cb38663332026ec1db382639cb609bb50a2d1b2
Jeremy <hajek@iit.edu>
2024-10-17 09:33:47 -0500

    adding additional test to sample

--- itmo-444-544/weekly-assignments/module-05/create-env-grader.py
+++ itmo-444-544/weekly-assignments/module-05/create-env-grader.py
@@ -1,4 +1,6 @@
 import boto3
+from tqdm import tqdm
+import requests
 
 clientEC2 = boto3.client('ec2')
 grandtotal = 0
@@ -22,3 +24,38 @@ else:
     print("You have  an incorrect number of regions: " + str(len(response['Regions'])) + "perhaps check your code where you declared number of regions...")
     currentPoints()
 
+##############################################################################
+# Test 5: Check PublicDNS and HTTP return status to check if webserver was 
+# installed and working
+##############################################################################
+print('*' * 79)
+print("Testing for the correct HTTP status (200) response from the webserver via the ELB URL...")
+# https://pypi.org/project/tqdm/
+for i in tqdm(range(30)):
+  time.sleep(1)
+
+
+checkHttpReturnStatusMismatch = False
+print("http://" + responseELB['LoadBalancers'][0]['DNSName'])
+try:
+  res=requests.get("http://" + responseELB['LoadBalancers'][0]['DNSName'])
+  if res.status_code == 200:
+    print("Successful request of the index.html file from " + str(responseELB['LoadBalancers'][0]['DNSName']))
+  else:
+    checkHttpReturnStatusMismatch = True
+    print("Incorrect http response code: " + str(res.status_code) + "from " + str(responseELB['LoadBalancers'][0]['DNSName']))
+except requests.exceptions.ConnectionError as errc:
+  print ("Error Connecting:",errc)
+  checkHttpReturnStatusMismatch = True
+  print("No response code returned - not able to connect: " + str(res.status_code) + "from " + str(responseELB['LoadBalancers'][0]['DNSName'])) 
+  sys.exit("Perhaps wait 1-2 minutes to let the install-env.sh server to finish installing the Nginx server and get the service ready to except external connections? Rerun this test script.")
+
+if checkHttpReturnStatusMismatch == False:
+   print("Correct HTTP status code of 200, received for the Elastic Load Balancer URL...")
+   grandtotal += 1
+   currentPoints()
+else:
+   print("Incorrect status code received. Perhaps go back and take a look at the --user-file value and the content of the install-env.sh script. Or check your security group to make sure the correct ports are open.")
+
+print('*' * 79)
+print("\r")
