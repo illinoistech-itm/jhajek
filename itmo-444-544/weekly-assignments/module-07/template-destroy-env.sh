d954c2054edbfda31d775244b6d01ee1102aef86
Jeremy <hajek@iit.edu>
2024-11-07 09:09:09 -0600

    update module-07

--- itmo-444-544/weekly-assignments/module-07/template-destroy-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-######################################################################
-# Since all elements have been created no need to send any inputs 
-# use the --query to get all of the needed IDs and reverse everything
-# you did in the create-env.sh 
-# you may need to use WAIT in some steps
-######################################################################
-
