7edebd8cbf869f0fc23980c1c6638b7412c7135c
Jeremy <hajek@iit.edu>
2024-11-19 09:25:19 -0600

    Co-authored-by: Rahul Nagaraju <rahulnagaraju724@users.noreply.github.com> Co-authored-by: KaushalNai <KaushalNai@users.noreply.github.com> Co-authored-by: Kevin-Alexis-Aguilar <Kevin-Alexis-Aguilar@users.noreply.github.com> Co-authored-by: divyeshh <divyeshh@users.noreply.github.com>

---
+++ itmo-444-544/weekly-assignments/module-08/install-env.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# module-08 sample code
+
+# Add comment and link describing NodeJs install
+# Installing Nodejs 22
+
+# Node JS Package List URL Link: https://github.com/nodesource/distributions?tab=readme-ov-file#using-debian-as-root-nodejs-22
+sudo apt-get install -y curl
+
+# Get the URL of the package
+curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+
+sudo -E bash nodesource_setup.sh
+
+# Install Node JS
+sudo apt-get install -y nodejs
+
+# Print out output
+node -v
+
+##############################################################################
+# Use NPM (node package manager to install AWS JavaScript SDK)
+##############################################################################
+# Run NPM to install the NPM Node packages needed for the code
+# You will start this NodeJS script by executing the command: node app.js
+# from the directory where app.js is located. The program `pm2` can be
+# used to auto start NodeJS applications (as they don't have a normal
+# systemd service handler).
+# <https://pm2.keymetrics.io/docs/usage/quick-start/>. This will require
+# the install of PM2 via npm as well.
+cd /home/ubuntu
+# sudo -u ubuntu npm install @aws-sdk/client-dynamodb @aws-sdk/client-sqs @aws-sdk/client-s3 @aws-sdk/client-sns express multer multer-s3 uuid ip
+
+# Install necessary libraries for our application
+sudo -u ubuntu npm install  @aws-sdk/client-s3 @aws-sdk/secrets-client-manager express multer multer-s3 uuid ip
+
+sudo npm install pm2 -g
+
+# Get your source code (index.html and app.js) on to each EC2 instance
+# So we can serve the provided index.html not the default "welcome to Nginx"
+
+# Change URL to your private repo
+sudo -u ubuntu git clone git@github.com:illinoistech-itm/hajek.git
+
+# Adjust repo name and path accordingly :-)
+sudo cp hajek/itmo-444-544/weekly-assignments/module-07/index.html /var/www/html/
+sudo cp hajek/itmo-444-544/weekly-assignments/module-07/app.js /var/www/html/
+
