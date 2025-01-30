b7efeceb6ed8bda5053fc74b00a669c5bc0977f5
Jeremy <hajek@iit.edu>
2025-01-30 16:11:34 -0600

    add code for sparksession create and spark.read()

--- itmd-521/labs/module-03/mnmcount.py
+++ itmd-521/labs/module-03/mnmcount.py
@@ -1,3 +1,13 @@
 import sys
 
 from pyspark.sql import SparkSession
+
+spark = (SparkSession
+         .builder
+         .appName("PythonMnMCount")
+         .getOrCreate())
+
+mnm_file = sys.argv[1]
+
+mnm_df = (spark.read.format("csv").option("header","true").option("inferSchema","true").load(mnm_file))
+
