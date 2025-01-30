f57547c11ea38fc4ee00d470289a2c5cb7d2344e
Jeremy <hajek@iit.edu>
2025-01-30 16:02:21 -0600

    initialize first commit

--- itmd-521/labs/module-03/mnmcount.py
+++ itmd-521/labs/module-03/mnmcount.py
@@ -1,25 +1,3 @@
 import sys
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import count
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        sys.exit(-1)
-
-    spark = (SparkSession.builder.appName("Python MnM Count").getOrCreate())
-
-    mnm_file = sys.argv[1]
-
-    mnm_df = (spark.read.format("csv").option("header","true").option("inferSchema", "true").load(mnm_file))
-
-    count_mnm_df = (mnm_df.select("State", "Color","Count").groupBy("State","Color").agg(count("Count").alias("Total")).orderBy("Total", ascending=False))
-
-    count_mnm_df.show(n=60, truncate=False)
-    print("Total Rows = %d" % (count_mnm_df.count()))
-
-    ca_count_mnm_df = (mnm_df.select("State","Color","Count").where(mnm_df.State == "CA").agg(count("Count").alias("Total")).orderBy("Total", ascending=False))
-
-    ca_count_mnm_df.show(n=10, truncate=False)
-
-    spark.stop()
