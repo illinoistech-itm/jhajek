051ffed33da8b1088d0bdd2612064097aaa8b286
Jeremy <hajek@iit.edu>
2025-01-30 16:15:27 -0600

    add spark.stop()

--- itmd-521/labs/module-03/mnmcount.py
+++ itmd-521/labs/module-03/mnmcount.py
@@ -11,3 +11,12 @@ mnm_file = sys.argv[1]
 
 mnm_df = (spark.read.format("csv").option("header","true").option("inferSchema","true").load(mnm_file))
 
+count_mnm_df = (mnm_df
+                .select("State","Color","Count")
+                .groupBy("State","Color")
+                .sum("Count")
+                .orderBy("sum(Count)", ascending=False))
+
+count_mnm_df.show(n=10, truncated=False)
+
+spark.stop()
