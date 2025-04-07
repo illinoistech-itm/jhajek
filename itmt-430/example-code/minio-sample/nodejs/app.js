2c750f241e65fe153bae746a1114c588663b3f0a
Jeremy <hajek@iit.edu>
2025-04-07 15:42:49 -0500

    update printing out values

--- itmt-430/example-code/minio-sample/nodejs/app.js
+++ itmt-430/example-code/minio-sample/nodejs/app.js
@@ -54,7 +54,7 @@ try {
         await minioClient.makeBucket(bucketName)
         console.log('Bucket created successfully.')
     }
- 
+    console.log(req.files.destination + req.files.filename)
     const fileStream = Fs.createReadStream(req.files.destination + req.files.filename)
     const fileStat = Fs.stat(req.files.destination + "/" + req.files.filename, function (err, stats) {
       if (err) {
