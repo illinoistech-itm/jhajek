5f1fd4e8f88a5319e5e5aa9fd0f119b81efb51c5
Jeremy <hajek@iit.edu>
2025-04-07 09:52:12 -0500

    update path

--- itmt-430/example-code/minio-sample/nodejs/app.js
+++ itmt-430/example-code/minio-sample/nodejs/app.js
@@ -250,7 +250,7 @@ try {
 //
 
 app.get("/", function (req, res) {
-res.sendFile(__dirname + "/index.html");
+res.sendFile(path.join(__dirname , "index.html"));
 });
 
 app.get("/gallery", function (req, res) {
