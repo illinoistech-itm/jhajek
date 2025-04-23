893b8f2e3583a5740d35f41b268d0727fd36227c
Jeremy <hajek@iit.edu>
2025-04-23 18:58:41 -0500

    update match faces

---
+++ itmo-444-544/live-session/match-faces.py
@@ -0,0 +1,20 @@
+import boto3
+
+client = boto3.client('rekognition')
+
+response = client.compare_faces(
+    SourceImage={
+        'S3Object': {
+            'Bucket': 'live-session',
+            'Name': 'P12.jpg'
+        }
+    },
+    TargetImage={
+        'S3Object': {
+            'Bucket': 'live-session',
+            'Name': 'P13.jpg'
+        }
+    }
+)
+
+print(response)
