14ab393982ebc63c86958b5a8744bd5e3d3e67a9
Jeremy <hajek@iit.edu>
2025-07-01 11:59:50 -0500

    add map public IP on launch to subnets

--- itmo-463/module-03/main.tf
+++ itmo-463/module-03/main.tf
@@ -86,6 +86,7 @@ resource "aws_subnet" "subneta" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = var.az[0]
+  map_public_ip_on_launch = true
 
   tags = {
     Name = var.tag
@@ -96,6 +97,7 @@ resource "aws_subnet" "subnetb" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   availability_zone = var.az[1]
+  map_public_ip_on_launch = true
 
   tags = {
     Name = var.tag
@@ -106,6 +108,7 @@ resource "aws_subnet" "subnetc" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
   availability_zone = var.az[2]
+  map_public_ip_on_launch = true
 
   tags = {
     Name = var.tag
