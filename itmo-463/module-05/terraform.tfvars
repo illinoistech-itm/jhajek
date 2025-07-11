2a0714efc65b5e77348ac28e3afcb27a3a4502e3
Jeremy <hajek@iit.edu>
2025-07-10 21:23:21 -0500

    Create module 05

---
+++ itmo-463/module-05/terraform.tfvars
@@ -0,0 +1,8 @@
+# Recommend keeping this as a template and modify on the "server-side"
+# This way you are not accidentally committing secrets to version control
+
+instance-type          = "t2.micro"
+key-name               = "vagrant-463-key-pair"
+tag                    = "module-05"
+tg-name                = "tf-example-lb-tg"
+elb-name               = "production-lb-tf"
---
+++ itmo-463/module-05/variables.tf
@@ -0,0 +1,20 @@
+# Terraform has datatypes, like a programming language we need to define our variables
+# We can have default values or define at run time or via a terraform.tfvars file
+
+
+# https://developer.hashicorp.com/terraform/language/expressions/types
+# string: a sequence of Unicode characters representing some text, like "hello".
+# number: a numeric value. The number type can represent both whole numbers like 15 and fractional values like 6.283185.
+# bool: a boolean value, either true or false. bool values can be used in conditional logic.
+# list (or tuple): a sequence of values, like ["us-west-1a", "us-west-1c"]. Identify elements in a list with consecutive whole numbers, starting with zero.
+# set: a collection of unique values that do not have any secondary identifiers or ordering.
+# map (or object): a group of values identified by named labels, like {name = "Mabel", age = 52}.
+
+# Default types are stings, lists, and maps
+
+variable "instance-type" {}
+variable "key-name" {}
+variable "az" { default = ["us-east-2a", "us-east-2b", "us-east-2c"] }
+variable "tag" {}
+variable "tg-name" {}
+variable "elb-name" {}
