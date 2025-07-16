a55c1afa55ff6729db81005644d2d091765f5444
Jeremy <hajek@iit.edu>
2025-07-16 15:02:36 -0500

    update removign old tempaltes

--- itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/jammy-grafana/post_install_prxmx_ubuntu_grafana_server.sh
+++ itmt-430/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/jammy-grafana/post_install_prxmx_ubuntu_grafana_server.sh
@@ -10,7 +10,7 @@ set -v
 sudo apt-get install -y apt-transport-https software-properties-common wget
 sudo mkdir -p /etc/apt/keyrings/
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 # Updates the list of available packages
 sudo apt-get update
 # Installs the latest OSS release:
--- itmt-430/example-code/proxmox-cloud-production-templates/terraform/proxmox-jammy-grafana/main.tf
+++ itmt-430/example-code/proxmox-cloud-production-templates/terraform/proxmox-jammy-grafana/main.tf
@@ -43,7 +43,7 @@ data "vault_generic_secret" "target_node" {
 resource "proxmox_vm_qemu" "grafana" {
   count = var.numberofvms
   name  = "${var.yourinitials}-vm${count.index}.service.consul"
-  desc  = var.lb-desc
+  desc  = var.desc
   #target_node = data.vault_generic_secret.target_node.data[random_shuffle.nodename.result[0]]
   target_node = random_shuffle.nodename.result[0]
   clone       = var.template_to_clone
