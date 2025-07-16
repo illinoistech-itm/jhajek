6656ff58a5d89d9758d337ab2b52e3f9a5cf5540
Jeremy <hajek@iit.edu>
2025-07-16 12:41:22 -0500

    update random node choice

--- itmt-430/example-code/proxmox-cloud-production-templates/terraform/proxmox-jammy-prometheus-server/main.tf
+++ itmt-430/example-code/proxmox-cloud-production-templates/terraform/proxmox-jammy-prometheus-server/main.tf
@@ -44,7 +44,8 @@ resource "proxmox_vm_qemu" "prometheus-server" {
   count       = var.numberofvms
   name        = "${var.yourinitials}-vm${count.index}.service.consul"
   desc        = var.desc
-  target_node = data.vault_generic_secret.target_node.data[random_shuffle.nodename.result[0]]
+  target_node = random_shuffle.nodename.result[0]
+  #target_node = data.vault_generic_secret.target_node.data[random_shuffle.nodename.result[0]]
   clone       = var.template_to_clone
   os_type     = "cloud-init"
   memory      = var.memory
