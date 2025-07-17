4c2e5250c5422c5c06e6c6d6fda78ffc69104129
Jeremy <hajek@iit.edu>
2025-07-17 12:00:26 -0500

    update adding temporary AWS instance for creating database image

--- itmo-463/module-06/M6-setup/main.tf
+++ itmo-463/module-06/M6-setup/main.tf
@@ -96,6 +96,38 @@ resource "aws_db_subnet_group" "project" {
   }
 }
 
+###############################################################################
+# Block to create AWS EC2 instance
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+##############################################################################
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "db_setup" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance-type
+  key_name = var.key-name
+  #vpc_security_group_ids = [aws_security_group.allow_module_04.id]
+  subnet_id = aws_subnet.example[1].id
+
+  tags = {
+    Name = var.tag-name
+  }
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_instance
 resource "aws_db_instance" "default" {
   depends_on = [ aws_secretsmanager_secret_version.project_password, aws_secretsmanager_secret_version.project_username ]
