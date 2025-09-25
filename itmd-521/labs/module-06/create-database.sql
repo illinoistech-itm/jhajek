7dafe23c704fb0e20c7cfad97a117595275b2929
Jeremy <hajek@iit.edu>
2025-09-25 17:43:17 -0500

    update correct name of database -- cannot have a '-'

--- itmd-521/labs/module-06/create-database.sql
+++ itmd-521/labs/module-06/create-database.sql
@@ -1,3 +1,3 @@
 --  This command will create a database named comments
 
-CREATE DATABASE module-06 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE itmd521 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
