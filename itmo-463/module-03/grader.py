1ab965c7470feecce227ccb55478044ea7f2c9d1
Jeremy <hajek@iit.edu>
2026-01-29 14:40:32 -0600

    Co-authored-by: CesarR2005 <CesarR2005@users.noreply.github.com>

---
+++ itmo-463/module-03/grader.py
@@ -0,0 +1,9 @@
+import boto3
+
+# link to boto resources: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html
+
+client_ec2 = boto3.client('ec2')
+
+response = client_ec2.describe_instances()
+
+print(response)
