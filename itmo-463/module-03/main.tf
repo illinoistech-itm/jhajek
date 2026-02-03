89561c5de7fb45b8e48303632601b626f85e5489
Jeremy <hajek@iit.edu>
2026-02-03 14:34:37 -0600

    remove filter

--- itmo-463/module-03/main.tf
+++ itmo-463/module-03/main.tf
@@ -4,11 +4,6 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
