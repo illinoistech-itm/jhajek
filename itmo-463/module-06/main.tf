22c2f5807f71f64c31bfa006c07062b403d7825a
Jeremy <hajek@iit.edu>
2026-02-12 14:58:43 -0600

    update route tables

--- itmo-463/module-06/main.tf
+++ itmo-463/module-06/main.tf
@@ -67,3 +67,30 @@ resource "aws_subnet" "us-east-2c" {
     Name = var.item_tag
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.item_tag
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    cidr_block        = aws_vpc.main.cidr_block
+    gateway_id = "local"
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
