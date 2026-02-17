18f177cc31b3fed358fd3c385ff9728e43b560a2
Jeremy <hajek@iit.edu>
2026-02-17 14:18:33 -0600

    update module 07

--- itmo-463/module-07/main.tf
+++ itmo-463/module-07/main.tf
@@ -10,7 +10,7 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
-
+  
 resource "aws_instance" "example" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
