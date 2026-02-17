ca079b7eba7e6ea8932b79ab0fd9ee2ca28750b7
Jeremy <hajek@iit.edu>
2026-02-17 14:52:42 -0600

    remove uneeded data and cur and paste old name

--- itmo-463/module-07/main.tf
+++ itmo-463/module-07/main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "example" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.project.id]
+  vpc_security_group_ids = [aws_security_group.project.id]
   subnet_id = aws_subnet.us-east-2a.id
   user_data = "./install-env.sh"
 
@@ -54,7 +54,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.project.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
