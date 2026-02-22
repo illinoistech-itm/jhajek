d2071a11b23d3dd54b28ec9007422b327de90ef1
Jeremy <hajek@iit.edu>
2026-02-22 14:45:37 -0600

    update to download the consul

--- itmt-430/example-code/vault-templates/packer/scripts/proxmox/core-noble/post_install_prxmx_install_hashicorp_consul.sh
+++ itmt-430/example-code/vault-templates/packer/scripts/proxmox/core-noble/post_install_prxmx_install_hashicorp_consul.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # script to install hashicorp consul for Proxmox servers
-
-wget -O- http://10.0.0.40/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+# https://developer.hashicorp.com/consul/install
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 #echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] http://10.0.0.40/hashicorp $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt-get update && sudo apt-get install -y consul
 
 sudo systemctl stop consul.service
