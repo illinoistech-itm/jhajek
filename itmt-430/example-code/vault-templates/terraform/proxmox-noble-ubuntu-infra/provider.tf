8ef6c2d11ffc8ed31fe63c5c83c2c7dfed9f9de3
Jeremy <hajek@iit.edu>
2026-02-24 11:15:06 -0600

    update correect syntax

--- itmt-430/example-code/vault-templates/terraform/proxmox-noble-ubuntu-infra/provider.tf
+++ itmt-430/example-code/vault-templates/terraform/proxmox-noble-ubuntu-infra/provider.tf
@@ -21,7 +21,7 @@ provider "vault" {}
 
 provider "proxmox" {
   pm_tls_insecure     = true
-  pm_api_url          = data.vault_generic_secret.pm_api_url.data["NODENAME1"]
+  pm_api_url          = data.vault_generic_secret.pm_api_url.data["NODE1"]
   pm_api_token_id     = data.vault_generic_secret.pm_api_token_id.data["TF-USERNAME"]
   pm_api_token_secret = data.vault_generic_secret.pm_api_token_secret.data["TF-TOKEN"]
   pm_log_enable       = var.pm_log_enable
