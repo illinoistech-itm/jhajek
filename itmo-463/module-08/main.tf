2099aefcacdeef72d80ea295fc1551839912343f
Jeremy <hajek@iit.edu>
2026-02-24 14:30:36 -0600

    remove run_instance

--- itmo-463/module-08/main.tf
+++ itmo-463/module-08/main.tf
@@ -31,30 +31,19 @@ data "aws_key_pair" "key_pair" {
 resource "aws_launch_template" "lt" {
   name = "lt-project"
   image_id = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
   placement {
     availability_zone = "us-east-2a"
   }
   user_data= filebase64("./install-env.sh")
   vpc_security_group_ids = [aws_security_group.project.id]
+  key_name = data.aws_key_pair.key_pair.key_name
   tag_specifications {
     resource_type = "instance"
 
     tags = {
       Name = var.item_tag
     }
-  }
-}
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.project.id]
-  subnet_id = aws_subnet.us-east-2a.id
-  user_data_base64 = filebase64("./install-env.sh")
-  key_name = data.aws_key_pair.key_pair.key_name
-
-  tags = {
-    Name = var.item_tag
   }
 }
 
