89fcef5d074432a8e758194dabbf66c7831eb489
Jeremy <hajek@iit.edu>
2026-02-28 12:43:11 -0600

    update bnehavior for ubuntu 24.04

--- itmt-430/example-code/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_create_service_account_for_flask_app.sh
+++ itmt-430/example-code/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_create_service_account_for_flask_app.sh
@@ -5,4 +5,9 @@ set -v
 # https://copilot.microsoft.com/shares/Pxz7LVbyQtyLZ4on86zkM
 
 echo "Create system account and group flaskuser ..."
-sudo adduser --system --group flaskuser
+# Using Ubuntu 22.04 this syntax creates a home directory by default: 
+# /home/flaskuser
+#sudo adduser --system --group flaskuser
+# Using Ubuntu 24.04 default behavior changed to not create a home directory
+# for a system account
+sudo adduser --system --home /home/flaskuser --group flaskuser
