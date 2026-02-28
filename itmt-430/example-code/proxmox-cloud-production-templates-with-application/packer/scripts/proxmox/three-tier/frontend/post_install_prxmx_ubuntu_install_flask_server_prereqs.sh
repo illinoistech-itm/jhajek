0ca77c10af0de56737ffe8649b32731846da01df
Jeremy <hajek@iit.edu>
2026-02-28 10:19:33 -0600

    update pre-reqs and names of files

--- itmt-430/example-code/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_install_flask_server_prereqs.sh
+++ itmt-430/example-code/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_install_flask_server_prereqs.sh
@@ -15,8 +15,10 @@ sudo apt install -y python3-setuptools python3-pip python3-dev
 sudo apt install -y gunicorn python3-flask
 
 # Requirements for Flask app functionality
-# python3-toml is needed since we are using Ubuntu Server 22.04.5 which ships Python 3.10
-sudo apt install -y python3-flask-socketio python3-flask-login python3-requests python3-requests-oauthlib python3-hvac python3-dotenv python3-paramiko python3-jinja2 python3-metaconfig python3-flask-sqlalchemy python3-toml python3-pymysql libmysqlclient-dev
+sudo apt install -y python3-flask-socketio python3-flask-login python3-requests python3-requests-oauthlib python3-hvac python3-dotenv python3-paramiko python3-jinja2 python3-metaconfig python3-flask-sqlalchemy 
+
+# Pre-reqs for MySQL database, if using PostgreSQL change these out
+sudo apt install -y python3-pymysql libmysqlclient-dev
 
 # Install dependencies for application logging to the Journal
 sudo apt install -y libsystemd-dev python3-systemd
