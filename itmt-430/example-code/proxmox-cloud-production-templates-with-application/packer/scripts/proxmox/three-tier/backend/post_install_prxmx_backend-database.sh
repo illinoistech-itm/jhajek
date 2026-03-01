83b3307c00bfafa964d2436965d68adf6eb65f26
Jeremy <hajek@iit.edu>
2026-03-01 15:04:15 -0600

    update include starting the mariadb service

--- itmt-430/example-code/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/backend/post_install_prxmx_backend-database.sh
+++ itmt-430/example-code/proxmox-cloud-production-templates-with-application/packer/scripts/proxmox/three-tier/backend/post_install_prxmx_backend-database.sh
@@ -7,5 +7,6 @@ sudo apt install -y mariadb-server
 
 # Required to have the mariadb.service start at boot time
 sudo systemctl enable mariadb.service
+sudo systemctl start mariadb.service
 ## During the Terraform apply phase -- we will make some run time adjustments
 # to configure the database to listen on the meta-network interface only
