16cd0d6666d522895e36477ccfa4edc17530a204
Jeremy <hajek@iit.edu>
2026-03-06 12:50:01 -0600

    add ig

--- terraform-challenge/main.tf
+++ terraform-challenge/main.tf
@@ -1,3 +1,12 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.item_tag
+  }
+}
