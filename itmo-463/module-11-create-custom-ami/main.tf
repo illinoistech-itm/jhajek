953e502c20c12fc6fd2be06f181604fa5fae7413
Jeremy <hajek@iit.edu>
2026-04-07 10:59:58 -0500

    update adding custom AMI code

---
+++ itmo-463/module-11-create-custom-ami/main.tf
@@ -0,0 +1,20 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+
+data "aws_instance" "ubuntu" {
+  filter {
+    name = "tag:Name"
+    values = [var.item_tag]
+  }
+}
+
+
+resource "aws_ami_from_instance" "custom_ami" {
+  name               = "project"
+  source_instance_id = data.aws_instance.ubuntu.id
+  snapshot_without_reboot = false
+
+  tags = {
+    Name = var.item_tag,
+    Type = "CustomAMI"
+  }
+}
--- itmo-463/module-11-image/main.tf
+++ itmo-463/module-11-image/main.tf
@@ -16,19 +16,6 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-##############################################################################
-# Data block to retrieve key pair name with particular filter
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/key_pair
-##############################################################################
-data "aws_key_pair" "key_pair" {
-
-    filter {
-    name = "tag:Name"
-    values = [var.item_tag]
-  }
-}
-
-
 resource "aws_instance" "example" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
@@ -39,16 +26,5 @@ resource "aws_instance" "example" {
 
   tags = {
     Name = var.item_tag
-  }
-}
-
-resource "aws_ami_from_instance" "custom_ami" {
-  name               = "project"
-  source_instance_id = aws_instance.example.id
-  snapshot_without_reboot = false
-
-  tags = {
-    Name = var.item_tag,
-    Type = "CustomAMI"
   }
 }
