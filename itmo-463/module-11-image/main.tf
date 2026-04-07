f73350817bbd31eee7ebbe98017b36cac3491b65
Jeremy <hajek@iit.edu>
2026-04-07 14:44:18 -0500

    Co-authored-by: CesarR2005 <CesarR2005@users.noreply.github.com>

--- itmo-463/module-11-image/main.tf
+++ itmo-463/module-11-image/main.tf
@@ -19,12 +19,41 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "example" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  #vpc_security_group_ids = [aws_security_group.project.id]
+  vpc_security_group_ids = [aws_security_group.project.id]
   #subnet_id = aws_subnet.us-east-2a.id
   #user_data_base64 = filebase64("./install-env.sh")
-  #key_name = data.aws_key_pair.key_pair.key_name
+  key_name = data.aws_key_pair.key_pair.key_name
 
   tags = {
     Name = var.item_tag_template
   }
 }
+
+##############################################################################
+# Create Security Group and create rules for port 22 access
+#
+##############################################################################
+
+resource "aws_security_group" "project" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = var.item_tag
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.project.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.project.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
