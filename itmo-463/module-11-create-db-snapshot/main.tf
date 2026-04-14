f1e36b9c24f107ee3644fe185ffb24db8f800e63
Jeremy <hajek@iit.edu>
2026-04-14 15:05:06 -0500

    Co-authored-by: CesarR2005 <CesarR2005@users.noreply.github.com> Co-authored-by: zeal0u5 <zeal0u5@users.noreply.github.com> Co-authored-by: kprajapati3108 <kprajapati3108@users.noreply.github.com>

--- itmo-463/module-11-create-db-snapshot/main.tf
+++ itmo-463/module-11-create-db-snapshot/main.tf
@@ -1,5 +1,20 @@
 # We need two blocks
 
 # 1 data block to retrieve our RDS instance ID
+##############################################################################
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/db_instance
+##############################################################################
+
+data "aws_db_instance" "database" {
+  tags = [var.item_tag_template]
+
+}
 
 # 1 resource block to create a DB snapshot - with tag of that RDS instance
+resource "aws_db_snapshot" "" {
+  db_instance_identifier = data.aws_db_instance.database.db_instance_identifier
+  db_snapshot_identifier = "testsnapshot1"
+tags = {
+    Name = var.item_tag
+  }
+}
