d51bdfdb1c80fff06fc3d7f70ca8ff2900281163
Jeremy <hajek@iit.edu>
2026-04-16 14:57:04 -0500

    update sudo clone ath

--- itmo-463/module-11-snapshot/install-env.sh
+++ itmo-463/module-11-snapshot/install-env.sh
@@ -5,3 +5,8 @@ sudo apt install -y nginx
 
 sudo systemctl enable nginx
 sudo systemctl start nginx
+
+
+# Cloning private repo over ssh
+echo "Beginning clone operation..."
+sudo -u ubuntu git clone git@github.com:illinoistech-itm/hajek.git /home/ubuntu/hajek
