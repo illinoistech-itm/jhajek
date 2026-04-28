bdf4bd4d0e81d459ed75d93e43d0eff66d8982f8
Jeremy <hajek@iit.edu>
2026-04-28 14:51:37 -0500

    update logic for starting application

--- itmo-463/module-12/install-env.sh
+++ itmo-463/module-12/install-env.sh
@@ -40,15 +40,15 @@ sudo npm install pm2 -g
 ####################################################################
 # Note - change "hajek.git" to be your private repo name (hawk ID) #
 ####################################################################
-sudo -u ubuntu git clone git@github.com:jhajek/coursera-cloud-computing.git
+sudo -u ubuntu git clone git@github.com:illinoistech-itm/jhajek.git
 
 # Start the nodejs app where it is located via PM2
 # https://pm2.keymetrics.io/docs/usage/quick-start
-cd /home/ubuntu/coursera-cloud-computing/itmo-463-563/M4L8
+cd /home/ubuntu/jhajek/itmo-463/module-12
 # Pull latest changes
 #sudo -u ubuntu git pull
 
-sudo cp /home/ubuntu/coursera-cloud-computing/itmo-463-563/M4L8/default /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/jhajek/itmo-463/module-12/default /etc/nginx/sites-available/default
 sudo systemctl daemon-reload
 sudo systemctl restart nginx
 
